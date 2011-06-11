d255920f5a20cc4c5e38a515f69a836bfc43a7e0
Steve Barnes <steve@echo.id.au>
2011-06-11 17:39:36 +1000

    Added revised p_openssh tests (Bash vs Python) provided by Athmane.

--- tests/p_openssh/0-install_sshd.sh
+++ tests/p_openssh/0-install_sshd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
-# SSH
-t_InstallPackage openssh-server
+# expect is needed by the test
+t_InstallPackage openssh-server expect
 chkconfig sshd on
 t_ServiceControl sshd start
---
+++ tests/p_openssh/sshd_user_login.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+
+userdel -rf sshtest; useradd sshtest && echo sshtest | passwd --stdin sshtest
+
+# Create a test file
+touch /home/sshtest/ssh_test_file
+
+t_Log "Running $0 - SSH user login test."
+
+./tests/p_openssh/_helper_sshd_user_login.expect | grep "ssh_test_file"
+ > /dev/null 2>&1
+
+t_CheckExitStatus $?
+
