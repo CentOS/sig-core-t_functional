26170b378dc4249da4f5bb313a8ae57403ad65a7
Athmane Madjoudj <athmane@fedoraproject.org>
2011-06-28 09:27:46 +0100

    Install packages required by the tests

--- tests/p_dovecot/0-install_dovecot.sh
+++ tests/p_dovecot/0-install_dovecot.sh
@@ -3,6 +3,9 @@
 
 t_Log "Running $0 - installation and startup of dovecot IMAP/POP3."
 
+# Install some pkgs needed by the tests
+t_InstallPackage nc grep
+
 t_InstallPackage dovecot
 chkconfig dovecot on
 t_ServiceControl dovecot start
--- tests/p_httpd/0-install_httpd.sh
+++ tests/p_httpd/0-install_httpd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
+# Install tests deps
+t_InstallPackage curl
+
 # HTTPD / PHP 
 t_InstallPackage httpd mod_ssl php php-mysql 
 chkconfig httpd on
