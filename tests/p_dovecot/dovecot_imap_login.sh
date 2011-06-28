7dcf2ad2ee7f765e92028085e3494d4d34a8a267
Fabian Arrotin <arrfab@centos.org>
2011-06-28 10:01:45 +0200

    Added a missing t_installPackage nc (nc was used during the test but not installed)

--- tests/p_dovecot/dovecot_imap_login.sh
+++ tests/p_dovecot/dovecot_imap_login.sh
@@ -5,6 +5,8 @@ t_Log "Running $0 - adding imaptest local user account + attempting IMAP login"
 
 { userdel imaptest; useradd imaptest && echo imaptest | passwd --stdin imaptest; } &>/dev/null
 
+t_InstallPackage nc
+
 t_Log "Dovecot IMAP login test"
 echo -e "01 LOGIN imaptest imaptest\n" | nc localhost 143 | grep "01 OK Logged in."
 
