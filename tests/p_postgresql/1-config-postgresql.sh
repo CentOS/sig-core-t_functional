e7a36a4e09d8d34758f0c51724ac507d319b47a4
Athmane Madjoudj <athmane@fedoraproject.org>
2011-06-28 18:49:47 +0100

    Fixed PostgreSQL tests on EL6

---
+++ tests/p_postgresql/1-config-postgresql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+
+if (t_GetPkgRel postgresql | grep -q el6)
+then
+   t_Log "Initialize PostgreSQL DB "     
+   service postgresql initdb
+   service postgresql restart
+else
+   t_Log "This script is not required for CentOS 5.x"
+fi 
