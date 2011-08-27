6e3dafb5d0676acae83603b213a96c364afa30aa
Athmane Madjoudj <athmane@fedoraproject.org>
2011-08-27 11:21:01 +0100

    Fix iptables default rules test

--- tests/p_iptables/iptables_default_rules.sh
+++ tests/p_iptables/iptables_default_rules.sh
@@ -3,12 +3,11 @@
 
 t_Log "Running $0 - check iptables default rules"
 
-(grep "-m state --state ESTABLISHED,RELATED -j ACCEPT" /etc/sysconfig/iptables > /dev/null 2>&1 ) && \
+(grep "state --state ESTABLISHED,RELATED -j ACCEPT" /etc/sysconfig/iptables > /dev/null 2>&1 ) && \
 
-(grep "-m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT" /etc/sysconfig/iptables > /dev/null 2>&1 ) && \
+(grep "state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT" /etc/sysconfig/iptables > /dev/null 2>&1 ) && \
 
-(grep "-j REJECT --reject-with icmp-host-prohibited"
- /etc/sysconfig/iptables > /dev/null 2>&1 )
+(grep "REJECT --reject-with icmp-host-prohibited" /etc/sysconfig/iptables > /dev/null 2>&1 )
 
 t_CheckExitStatus $?
 
