02ecd47167e317dbb94448c2a68e58464f1cc86c
Athmane Madjoudj <athmane@fedoraproject.org>
2011-08-27 11:11:33 +0100

    Fix SELinux alerts test

--- tests/p_selinux/selinux_alerts.sh
+++ tests/p_selinux/selinux_alerts.sh
@@ -3,7 +3,7 @@
 
 t_Log "Running $0 - check for SELinux alerts (AVC)"
 
-grep "AVC" /var/log/audit/audit.log > /dev/null 2>&1
+grep -v "AVC" /var/log/audit/audit.log > /dev/null 2>&1
 
 t_CheckExitStatus $?
 
