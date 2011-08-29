8f5b5fa2b1ed81a8467c9ac9bc45904811f39069
Fabian Arrotin <arrfab@centos.org>
2011-08-29 22:43:19 +0200

    Fixed a typo : t_log -> t_Log

--- tests/p_procinfo/5-test_procinfo.sh
+++ tests/p_procinfo/5-test_procinfo.sh
@@ -5,8 +5,8 @@ t_Log "Running $0 - checking procinfo runs and returns non-zero exit status."
 
 PROCINFO=`which procinfo`
 
-[ "$PROCINFO" ] || { t_log "Failed to find procinfo binary. Cannot continue."; exit $FAIL; }
+[ "$PROCINFO" ] || { t_Log "Failed to find procinfo binary. Cannot continue."; exit $FAIL; }
 
 $PROCINFO &>/dev/null
 
-[ $? -eq 0 ] || { t_log "Procinfo exited with non-zero status. That ain't good..."; exit $FAIL; }
+[ $? -eq 0 ] || { t_Log "Procinfo exited with non-zero status. That ain't good..."; exit $FAIL; }
