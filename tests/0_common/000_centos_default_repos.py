d7641357bb94b10482c2f4072adc23d2430243df
Karanbir Singh <kbsingh@karan.org>
2011-08-31 00:30:05 +0100

    when testing enabled repos, print the wrongly enabled one

--- tests/0_common/000_centos_default_repos.py
+++ tests/0_common/000_centos_default_repos.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+#         Karanbir Singh <kbsingh@karan.org>
 # Test default CentOS repos
 
 import yum
@@ -10,6 +11,7 @@ centos_default_repos = ['base','extras','updates']
 print "Check if non default repo is enabled"
 for repo in yb.repos.listEnabled():
     if not repo.id in centos_default_repos:
+        print '%s is enabled, should be disabled at this stage' % repo.id
         print '-> FAIL'
         sys.exit(1)
 print '-> PASS'
