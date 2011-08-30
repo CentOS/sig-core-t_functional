995589e349d5d39c8ecdbf6efca4f9c9bac4544d
Athmane Madjoudj <athmane@fedoraproject.org>
2011-08-30 11:54:30 +0100

    Fix Yum distroverpkg test on CentOS 5

--- tests/p_yum/yum_distroverpkg.sh
+++ tests/p_yum/yum_distroverpkg.sh
@@ -3,6 +3,11 @@
 
 t_Log "Running $0 - Yum configuration has the correct distroverpkg value test."
 
-grep "distroverpkg=centos-release" /etc/yum.conf >/dev/null 2>&1
+if (t_GetPkgRel basesystem | grep -q el6)
+then
+    grep "distroverpkg=centos-release" /etc/yum.conf >/dev/null 2>&1
+else
+    echo "Test skipped for CentOS <= 5"
+fi
 
 t_CheckExitStatus $?
