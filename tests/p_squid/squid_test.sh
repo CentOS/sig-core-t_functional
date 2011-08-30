97873c63b76caacc81997215f7e0f4f855a7bc07
Fabian Arrotin <arrfab@centos.org>
2011-08-30 14:12:18 +0200

    Fixed a missing / in the URL to fetch for the squid test

--- tests/p_squid/squid_test.sh
+++ tests/p_squid/squid_test.sh
@@ -3,6 +3,6 @@
 
 t_Log "Running $0 - Squid test."
 
-squidclient -T 2 http://repo.centos.qa/qa | grep "ks_cfg"  >/dev/null 2>&1
+squidclient -T 2 http://repo.centos.qa/qa/ | grep "ks_cfg"  >/dev/null 2>&1
 
 t_CheckExitStatus $?
