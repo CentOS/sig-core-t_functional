df32c47cf405d1267bbae2dfcc1e47ce66c845a6
Athmane Madjoudj <athmanem@gmail.com>
2011-09-17 23:12:30 +0100

    Add shells file sanity check.

---
+++ tests/p_setup/shells_file_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+
+t_Log "Running $0 - shells file sanity check."
+
+grep -q 'bash' /etc/shells && \
+grep -q 'nologin' /etc/shells
+
+
+t_CheckExitStatus $?
