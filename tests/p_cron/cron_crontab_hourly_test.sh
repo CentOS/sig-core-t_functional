dcd58cebf84febd6858d3942979492b9cc88d1e6
Athmane Madjoudj <athmanem@gmail.com>
2011-09-20 21:08:17 +0100

    Fix cron tests

--- tests/p_cron/cron_crontab_hourly_test.sh
+++ tests/p_cron/cron_crontab_hourly_test.sh
@@ -3,6 +3,13 @@
 
 t_Log "Running $0 - crontab test."
 
+# Add a test cron
+cat > /etc/cron.hourly/test.sh<<EOF
+#!/bin/sh
+echo 'test'
+EOF
+chmod +x /etc/cron.hourly/test.sh
+
 run-parts /etc/cron.hourly
 tail /var/log/cron | grep -q 'run-parts(/etc/cron.hourly)'
 
--- tests/p_cron/cron_crontab_weekly_test.sh
+++ tests/p_cron/cron_crontab_weekly_test.sh
@@ -3,6 +3,14 @@
 
 t_Log "Running $0 - crontab test."
 
+# Add a test cron
+cat > /etc/cron.weekly/test.sh<<EOF
+#!/bin/sh
+echo 'test'
+EOF
+
+chmod +x /etc/cron.weekly/test.sh
+
 run-parts /etc/cron.weekly
 tail /var/log/cron | grep -q 'run-parts(/etc/cron.weekly)'
 
