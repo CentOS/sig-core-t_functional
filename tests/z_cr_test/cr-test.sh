4ebcb55bb22bffc668e9df2dd1a02c76e6fdb379
Fabian Arrotin <arrfab@centos.org>
2011-10-12 16:59:55 -0500

    Added the CR test script, not called and launched by default

---
+++ tests/z_cr_test/cr-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# determining which packages we need to install from Base+Updates before trying the update against CR
+
+# generate local cache
+./0-common/00_qa_repo_config.sh
+yum --enablerepo=qa-cr list >/dev/null 2>&1 
+sqlite3 $(find /var/cache/yum/$(t_GetArch)/$(t_DistCheck)/cr/ -iname '*.sqlite*') 'select name from packages;' > /tmp/packages.list
+
+t_Log "Installing needed packages from Base"
+yum install -y -d0 $(cat /tmp/packages.list) 1>/dev/null
+t_CheckExitStatus $?
+
+t_Log "Updating packages with the CR repo"
+t_InstallPackage yum-presto
+yum install --enablerepo=qa-cr -y -d0 $(cat /tmp/packages.list) 
+t_CheckExitStatus $?
+
