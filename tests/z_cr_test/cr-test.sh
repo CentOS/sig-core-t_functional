be772ae8576b55987df35385fae8753f792023d5
Fabian Arrotin <arrfab@centos.org>
2011-10-14 21:18:23 -0500

    disabling the normal repositories for cr tests, then enabling again

--- tests/z_cr_test/cr-test.sh
+++ tests/z_cr_test/cr-test.sh
@@ -3,15 +3,19 @@
 
 # generate local cache
 /tmp/t_functional/tests/0_common/00_qa_repo_config.sh
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.disabled
 yum --enablerepo=qa-cr list >/dev/null 2>&1 
 sqlite3 $(find /var/cache/yum/$(t_GetArch)/$(t_DistCheck)/qa-cr/ -iname '*.sqlite*') 'select name from packages;' > /tmp/packages.list
-
 t_Log "Installing needed packages from Base"
 yum install -y -d0 $(cat /tmp/packages.list) 1>/dev/null
 t_CheckExitStatus $?
+# remove centos-release-cr from the packages list
+yum remove -y -d0 centos-release-cr
 
 t_Log "Updating packages with the CR repo"
 t_InstallPackage yum-presto
 yum install --enablerepo=qa-cr -y -d0 $(cat /tmp/packages.list) 
 t_CheckExitStatus $?
-
+yum remove -y -d0 centos-release-cr
+mv /etc/yum.repos.d/CentOS-Base.repo.disabled /etc/yum.repos.d/CentOS-Base.repo
+mv /etc/yum.repos.d/CentOS-QA.repo /etc/yum.repos.d/CentOS-QA.repo.disabled
