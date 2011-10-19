d51c128bdeebbd3d0e81df6ee7aff0fb57c315ed
Athmane Madjoudj <athmanem@gmail.com>
2011-10-19 21:49:16 +0100

    Add LSB branding test

---
+++ tests/p_lsb/lsb_release_brand_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+
+t_Log "Running $0 - LSB CentOS braning check."
+
+lsb_release -i | grep -q "CentOS" && \
+lsb_release -d | grep -q "CentOS"  
+
+t_CheckExitStatus $?
