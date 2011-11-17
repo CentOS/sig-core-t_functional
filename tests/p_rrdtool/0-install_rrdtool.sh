26fe2dee5908ecf3de76092944dc070f0740985d
Christoph Galuschka <christoph.galuschka@tiwag.at>
2011-11-17 13:44:00 +0100

    Added minicom and RRD-Tools tests

---
+++ tests/p_rrdtool/0-install_rrdtool.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Author: Christoph Galuschka <christoph.galuschka@tiwag.at>
+
+# Install rrdtool
+t_InstallPackage rrdtool
+
+
---
+++ tests/p_rrdtool/rrdtool_test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+# Author: Christph Galuschka <christoph.galuschka@tiwag.at>
+# RRD-sample from http://oss.oetiker.ch/rrdtool/doc/rrdcreate.en.html
+
+t_Log "Running $0 - basic rrdtool test."
+
+# creating test RRD
+
+FILE="/var/tmp/temperature.rrd"
+
+rrdtool create $FILE --step 1 DS:temp:GAUGE:600:-273:5000 RRA:AVERAGE:0.5:1:1200
+
+# insert value
+rrdtool update $FILE N:30
+
+# retrieve value and check
+WORKING=$(rrdtool fetch $FILE AVERAGE -s -80 |grep -c '3.0000000000e+01')
+
+# if $WORKING > 0 -> update and retrieval works
+if [ $WORKING > 0 ]; then ret_val=0;
+fi
+
+#remove RRD-File
+/bin/rm $FILE
+
+t_CheckExitStatus $ret_val
