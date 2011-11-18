63ebd1167c031c70e54fcbdd8ebcf68a4bc2982c
Christoph Galuschka <christoph.galuschka@tiwag.at>
2011-11-18 22:39:20 +0100

    Added functional test for php

---
+++ tests/p_php/php-test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+
+t_Log "Running $0 - php functional test."
+
+# Comparing hostname with output of phpinfo()
+HOST=$(hostname)
+FILE='/var/tmp/php-test.php'
+
+cat > $FILE <<EOF
+<?php
+phpinfo();
+?>
+EOF
+
+# setting timezone for phpinfo
+sed  -i 's/\;date\.timezone\ \=/date\.timezone = \"Europe\/Berlin\"/' /etc/php.ini
+
+php $FILE | grep -cq 'HOSTNAME => '$HOST
+
+t_CheckExitStatus $?
+
+#reversing changes
+/bin/rm $FILE
+sed  -i 's/\date\.timezone\ \=\ \"Europe\/Berlin\"/\;date\.timezone\ \=/' /etc/php.ini
