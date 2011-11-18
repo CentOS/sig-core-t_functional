d3a5ddf9c0c43c58df49170695bc0138e7ab353c
Athmane Madjoudj <athmanem@gmail.com>
2011-11-18 21:03:07 +0100

    Fix bind test on C5 as C5's bind does not have default config

--- tests/p_bind/0-install_bind.sh
+++ tests/p_bind/0-install_bind.sh
@@ -3,4 +3,35 @@
 
 t_InstallPackage bind bind-utils
 
+if (t_GetPkgRel basesystem | grep -q el5)
+then
+   cat > /etc/named.conf <<EOF
+options {
+        listen-on port 53 { 127.0.0.1; };
+        listen-on-v6 port 53 { ::1; };
+        directory       "/var/named";
+        dump-file       "/var/named/data/cache_dump.db";
+        statistics-file "/var/named/data/named_stats.txt";
+        memstatistics-file "/var/named/data/named_mem_stats.txt";
+        allow-query     { localhost; };
+        recursion yes;
+
+};
+
+zone "." IN {
+        type hint;
+        file "named.root";
+};
+
+include "/etc/named.rfc1912.zones";
+
+EOF
+
+
+    cp /usr/share/doc/bind-*/sample/etc/named.rfc1912.zones /etc/
+    cp -R /usr/share/doc/bind-*/sample/var/named/local* /var/named/
+    cp -R /usr/share/doc/bind-*/sample/var/named/named.* /var/named/
+    chown -R named:named /var/named/* /etc/named.*
+fi
+
 t_ServiceControl named start
