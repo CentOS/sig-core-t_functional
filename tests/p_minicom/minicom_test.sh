ec0734abad930adda81054306a044c3e52719579
Athmane Madjoudj <athmanem@gmail.com>
2011-11-18 21:20:49 +0100

    Add a FIXME in minicom test

--- tests/p_minicom/minicom_test.sh
+++ tests/p_minicom/minicom_test.sh
@@ -2,8 +2,9 @@
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 # Author: Christoph Galuschka <christoph.galuschka@chello.at>
 
-t_Log "Running $0 - very basic minicom test."
+t_Log "Running $0 - minicom binary load test."
 
+# FIXME: this test is very basic
 minicom -h | grep -q "terminal program"
 
 t_CheckExitStatus $?
