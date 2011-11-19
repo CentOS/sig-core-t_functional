2e82fdf39c834fff48223c27f3392477ed434408
Christoph Galuschka <christoph.galuschka@chello.at>
2011-11-19 19:02:14 +0100

    Added test for ICMPv6 to loopback

--- tests/p_tcpdump/tcpdump_lo.sh
+++ tests/p_tcpdump/tcpdump_lo.sh
@@ -2,21 +2,28 @@
 # Author: Christoph Galuschka <christoph.galuschka@chello.at>
 #         Athmane Madjoudj <athmanem@gmail.com>
 
-t_Log "Running $0 - TCPdump test"
+t_Log "Running $0 - TCPdump test to lo"
 
 #Dumping 4 pings to loopback to file
-tcpdump -q -n -p -i lo -w /var/tmp/centos_test.pcap &
+FILE='/var/tmp/lo_test.pcap'
+
+tcpdump -q -n -p -i lo -w $FILE &
 # If we don't wait a short time, the first paket will be missed by tcpdump
 sleep 1
 ping -q -c 4 -i 0.25 127.0.0.1
-killall -s SIGINT tcpdump
 sleep 1
+killall -s SIGINT tcpdump
 
 # reading from file, for each ping we should see two pakets
-WORKING=$( tcpdump -r /var/tmp/centos_test.pcap | grep -ci icmp )
-if [ $WORKING == 8 ]; then ret_val=0; fi
+WORKING=$( tcpdump -r $FILE | grep -ci icmp )
+if [ $WORKING == 8 ]
+  then
+  ret_val=0
+else
+  ret_val=1
+fi
 
 # Remove file afterwards
-# /bin/rm /var/tmp/centos_test.pcap
+/bin/rm $FILE
 
 t_CheckExitStatus $ret_val
