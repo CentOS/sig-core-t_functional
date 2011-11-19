adb04effd83ebaf1144401b0e999a501ac2a45af
Christoph Galuschka <christoph.galuschka@chello.at>
2011-11-19 19:22:49 +0100

    Added ping-test to Default-GW and improved test-Scripts to loopback

---
+++ tests/p_tcpdump/tcpdump_eth0.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+#         Athmane Madjoudj <athmanem@gmail.com>
+
+t_Log "Running $0 - TCPdump test to Default-GW with IPv4"
+
+# Grabing Default-Router of eth0
+IP=$(ip route list default | grep 'default via ')
+regex='.*via\ (.*)\ dev.*'
+if [[ $IP =~ $regex ]]
+  then
+  t_Log "Found Default-GW - starting tcpdump test"
+  #Dumping 4 pings via eth0 to file
+  FILE='/var/tmp/eth0_test.pcap'
+  COUNT='4'
+  tcpdump -q -n -p -i eth0 -w $FILE &
+  # If we don't wait a short time, the first paket will be missed by tcpdump
+  sleep 1
+  ping -q -c $COUNT -i 0.25 ${BASH_REMATCH[1]}
+  sleep 1
+  killall -s SIGINT tcpdump
+  # reading from file, for each ping we should see two pakets
+  WORKING=$( tcpdump -r $FILE | grep -ci icmp )
+  # The script will allways work, but if we the log does not contain
+  # what we expect, we will log it
+  if [ $WORKING == $[COUNT*2] ]
+    then
+    ret_val=0
+  else
+    t_Log "ping to Default-Gateway droped pakets!! Only "$WORKING" of "$[COUNT*2]" entries were found!!"
+    ret_val=1
+  fi
+else
+  t_Log "Found Default-GW - skiping test"
+  ret_val=0
+fi
+# Remove file afterwards
+/bin/rm $FILE
+
+t_CheckExitStatus $ret_val
--- tests/p_tcpdump/tcpdump_lo.sh
+++ tests/p_tcpdump/tcpdump_lo.sh
@@ -4,22 +4,24 @@
 
 t_Log "Running $0 - TCPdump test to lo"
 
-#Dumping 4 pings to loopback to file
+#Dumping pings to loopback to file
 FILE='/var/tmp/lo_test.pcap'
+COUNT='4'
 
 tcpdump -q -n -p -i lo -w $FILE &
 # If we don't wait a short time, the first paket will be missed by tcpdump
 sleep 1
-ping -q -c 4 -i 0.25 127.0.0.1
+ping -q -c $COUNT -i 0.25 127.0.0.1
 sleep 1
 killall -s SIGINT tcpdump
 
 # reading from file, for each ping we should see two pakets
 WORKING=$( tcpdump -r $FILE | grep -ci icmp )
-if [ $WORKING == 8 ]
+if [ $WORKING == $[COUNT*2] ]
   then
   ret_val=0
 else
+  t_Log "ping to loopback droped pakets!! This should not happen on loopback"
   ret_val=1
 fi
 
