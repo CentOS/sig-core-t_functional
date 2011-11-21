b45181f3b4a806effec7ab73897719dfcd0c784d
Athmane Madjoudj <athmanem@gmail.com>
2011-11-21 10:41:30 +0100

    Workaround iptables test

--- tests/p_iptables/iptables_default_rules.sh
+++ tests/p_iptables/iptables_default_rules.sh
@@ -3,11 +3,17 @@
 
 t_Log "Running $0 - check iptables default rules"
 
+if [ $SKIP_QA_HARNESS ]; then
+  echo "Skip, No standard firewall config ..."
+else
+
 (grep "state --state ESTABLISHED,RELATED -j ACCEPT" /etc/sysconfig/iptables > /dev/null 2>&1 ) && \
 
 (grep "state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT" /etc/sysconfig/iptables > /dev/null 2>&1 ) && \
 
 (grep "REJECT --reject-with icmp-host-prohibited" /etc/sysconfig/iptables > /dev/null 2>&1 )
+
+fi
 
 t_CheckExitStatus $?
 
