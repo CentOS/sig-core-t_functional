d80c6258761d24344c716a242efc9cc6661d6b48
Athmane Madjoudj <athmanem@gmail.com>
2011-11-22 20:13:55 +0100

    Don't start tomcat before it configured

--- tests/p_tomcat/0-install_tomcat.sh
+++ tests/p_tomcat/0-install_tomcat.sh
@@ -5,9 +5,7 @@ if (t_GetPkgRel basesystem | grep -q el6)
 then
    t_Log "$0 - installing Tomcat 6"
    t_InstallPackage  tomcat6 tomcat6-admin-webapps tomcat6-webapps
-   service tomcat6 start
 else
    t_Log "$0 - installing Tomcat 5"
    t_InstallPackage  tomcat5 tomcat5-admin-webapps tomcat5-webapps
-   service tomcat5 start
 fi
