43f4bcb0088dfcc625cdcf9af503ff3c0b696761
Athmane Madjoudj <athmanem@gmail.com>
2011-11-25 17:16:42 +0100

    Fix nfs test on C6

--- tests/p_nfs/0-install_nfs.sh
+++ tests/p_nfs/0-install_nfs.sh
@@ -5,6 +5,11 @@
 t_InstallPackage nfs-utils
 
 # Restart because usualy NFS is enabled by default on CentOS-5
-t_ServiceControl portmap restart
 t_ServiceControl nfs restart
 
+if (t_GetPkgRel basesystem | grep -q el6)
+then
+   t_ServiceControl rpcbind restart
+else
+   t_ServiceControl portmap restart
+fi
