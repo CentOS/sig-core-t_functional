0a47304118566be96cb24c07549af01fd7480758
Athmane Madjoudj <athmanem@gmail.com>
2011-11-26 12:38:11 +0100

    Add some notes

--- tests/p_kernel/kernel_centos_keyring.sh
+++ tests/p_kernel/kernel_centos_keyring.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+# Note: This was a known issue with CentOS 6.0 GA kernel
 
 t_Log "Running $0 -  check CentOS' Kernel Module GPG key."
 
--- tests/p_ntp/ntp_centos_servers.sh
+++ tests/p_ntp/ntp_centos_servers.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+# Ref.: http://bugs.centos.org/view.php?id=4943
 
 t_Log "Running $0 - NTP is using CentOS server pool test."
 
