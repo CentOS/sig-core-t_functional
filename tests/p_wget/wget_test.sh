c89712f7a11e88a2d4b5d8f1f99bd9f6848ac9ec
Fabian Arrotin <arrfab@centos.org>
2011-12-15 20:31:46 +0100

    Changed $HOST to $URL in the wget test to permit a full url

--- tests/p_wget/wget_test.sh
+++ tests/p_wget/wget_test.sh
@@ -6,16 +6,16 @@ t_Log "Running $0 - wget can access http-host and download index.html."
 FILE=/var/tmp/index.html
 
 if [ $SKIP_QA_HARNESS ]; then
-  HOST=wiki.centos.org
+  URL=http://wiki.centos.org/
   CHECK_FOR="FrontPage - CentOS Wiki"
 else
-  HOST=repo.centos.qa
+  URL=http://repo.centos.qa/srv/CentOS/
   # athmane: could you please insert something here that is returned from qa-host
   CHECK_FOR="CentOS"
 fi
 
 t_Log "Querying http://${HOST}"
-wget -q --output-document=${FILE} http://${HOST}/
+wget -q --output-document=${FILE} http://${URL}
 grep -q "${CHECK_FOR}" ${FILE}
 ret_val=$?
 
