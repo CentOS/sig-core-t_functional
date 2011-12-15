db9377f4bb3a76e80e042aed2342c12044aa0496
Fabian Arrotin <arrfab@centos.org>
2011-12-15 21:08:51 +0100

    Bad day to fix test during QA .. removed the extra http:// in front of ${URL}

--- tests/p_wget/wget_test.sh
+++ tests/p_wget/wget_test.sh
@@ -6,16 +6,16 @@ t_Log "Running $0 - wget can access http-host and download index.html."
 FILE=/var/tmp/index.html
 
 if [ $SKIP_QA_HARNESS ]; then
-  URL=http://wiki.centos.org/
+  URL="http://wiki.centos.org/"
   CHECK_FOR="FrontPage - CentOS Wiki"
 else
-  URL=http://repo.centos.qa/srv/CentOS/
+  URL="http://repo.centos.qa/srv/CentOS/"
   # athmane: could you please insert something here that is returned from qa-host
   CHECK_FOR="CentOS"
 fi
 
 t_Log "Querying http://${HOST}"
-wget -q --output-document=${FILE} http://${URL}
+wget -q --output-document=${FILE} ${URL}
 grep -q "${CHECK_FOR}" ${FILE}
 ret_val=$?
 
