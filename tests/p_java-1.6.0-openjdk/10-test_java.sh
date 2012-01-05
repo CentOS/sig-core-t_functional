e2cd204c14ed114e00a46a43e4893036d4948823
Christoph Galuschka <christoph.galuschka@chello.at>
2012-01-05 13:51:56 +0100

    new tests for javac and java

---
+++ tests/p_java-1.6.0-openjdk/10-test_java.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+
+t_Log "Running $0 - javac can compile and java can print 'hello centos'"
+
+# creating source file
+PATH2FILE='/var/tmp/'
+FILE='HelloWorld'
+FILE2=$PATH2FILE$FILE.java
+
+cat > $FILE2 <<EOF
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("hello centos");
+  }
+}
+EOF
+
+# Installing Java compiler
+t_Log "installing openjdk compiler."
+t_InstallPackage java-1.6.0-openjdk-devel
+
+# Compiling
+javac $FILE2
+if [ $? == 1 ]
+  then
+  t_Log "Java-Compilation failed"
+  exit
+fi
+
+# executing java
+workpath=$(pwd)
+cd $PATH2FILE
+java $FILE |grep -cq 'hello centos'
+
+t_CheckExitStatus $?
+
+cd $workpath
+# remove files
+/bin/rm $PATH2FILE$FILE.class $FILE2
