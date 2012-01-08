d6479eaa3607e8c3eeb5a5264d65ccb589bf099f
Athmane Madjoudj <athmanem@gmail.com>
2012-01-08 18:37:56 +0100

    Add whois tests

---
+++ tests/p_jwhois/0-install_jwhois.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+
+# nc (netcat) is required for the test
+t_InstallPackage  jwhois nc
+
---
+++ tests/p_jwhois/jwhois_test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Author: Athmane Madjoudj <athmanem@gmail.com>
+
+t_Log "Running $0 - check that jwhois can connect to whois server and get the info."
+
+# Dummy whois server
+echo 'tf_jwhois_test_response' | nc -l 43 &
+sleep 1
+
+whois -h 127.0.0.1 domain.tld | grep -q 'tf_jwhois_test_response'
+
+t_CheckExitStatus $?
