0e2392bc3be2ea5434c63b97af82e89f2d627e3a
Christoph Galuschka <christoph.galuschka@chello.at>
2012-03-24 19:03:45 +0100

    fixed dovecot login tests (hopefully)  enter the commit message for your changes. Lines starting

--- tests/p_dovecot/dovecot_imap_login.sh
+++ tests/p_dovecot/dovecot_imap_login.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+#         Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - adding imaptest local user account + attempting IMAP login"
 
 { userdel -rf imaptest; useradd imaptest && echo imaptest | passwd --stdin imaptest; } &>/dev/null
 
-t_Log "Dovecot IMAP login test"
-echo -e "01 LOGIN imaptest imaptest\n" | nc localhost 143 | grep "01 OK Logged in."
+# creating maildir in HOME, else test will fail at first try
+mkdir -m 700 -p /home/imaptest/mail/.imap/INBOX 
+chown -R imaptest:imaptest /home/imaptest/mail/.imap/INBOX
 
-if (t_GetPkgRel dovecot | grep -q el6)
-then
-   echo "[*] ** EXPERIMENTAL **: Test not working on CentOS 6, forcing PASS"
-   true 
-fi
+t_Log "Dovecot IMAP login test"
+echo -e "01 LOGIN imaptest imaptest\n" | nc localhost 143 | grep -q "Logged in."
 
 t_CheckExitStatus $?
 
--- tests/p_dovecot/dovecot_pop3_login.sh
+++ tests/p_dovecot/dovecot_pop3_login.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+#         Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - adding pop3test local user account + attempting POP3 login"
 
-{ userdel pop3test; useradd pop3test && echo pop3test | passwd --stdin pop3test; } &>/dev/null
+{ userdel -rf pop3test; useradd pop3test && echo pop3test | passwd --stdin pop3test; } &>/dev/null
+
+# creating maildir in HOME, else test will fail at first try
+mkdir -m 700 -p /home/pop3test/mail/.imap/INBOX 
+chown -R pop3test:pop3test /home/pop3test/mail/.imap/INBOX
 
 t_Log "Dovecot POP3 login test"
-echo -e "user pop3test\npass pop3test\n" | nc localhost 110 | grep "+OK Logged in."
-
-if (t_GetPkgRel dovecot | grep -q el6)
-then
-   echo "[*] ** EXPERIMENTAL **: Test not working on CentOS 6, forcing PASS"
-   true
-fi
+echo -e "user pop3test\npass pop3test\n" | nc localhost 110 | grep -q "+OK Logged in."
 
 t_CheckExitStatus $?
 
