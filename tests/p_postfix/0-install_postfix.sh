2844c1a89666356f7aa9c4e11c5284ebe44a6ad0
Christoph Galuschka <christoph.galuschka@chello.at>
2012-05-12 22:16:54 +0200

    fix for MTA-tests on C5

--- tests/p_postfix/0-install_postfix.sh
+++ tests/p_postfix/0-install_postfix.sh
@@ -3,5 +3,16 @@
 
 # Postfix
 t_InstallPackage postfix
+
+# Removing other MTA
+t_ServiceControl sendmail stop
+sleep 3
 t_RemovePackage sendmail
+if (t_GetPkgRel basesystem | grep -q el5)
+then
+  t_ServiceControl exim stop
+  sleep 3
+  t_RemovePackage exim
+fi
+
 t_ServiceControl postfix start
--- tests/p_sendmail/0-install_sendmail.sh
+++ tests/p_sendmail/0-install_sendmail.sh
@@ -7,5 +7,12 @@ t_InstallPackage sendmail
 t_ServiceControl postfix stop
 sleep 3
 t_RemovePackage postfix
+if (t_GetPkgRel basesystem | grep -q el5)
+then
+  #also stoping/removing exim
+  t_ServiceControl exim stop
+  sleep 3
+  t_RemovePackage exim
+fi
 
 t_ServiceControl sendmail start
