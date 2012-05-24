a8204fd91ac59a87f8a83d1a9acc13fa33532503
Christoph Galuschka <christoph.galuschka@chello.at>
2012-05-24 21:04:21 +0200

    fix for cracklib test on C5

--- tests/p_cracklib/cracklib_passwordtest.sh
+++ tests/p_cracklib/cracklib_passwordtest.sh
@@ -12,9 +12,14 @@ t_Log "checking simple password"
 echo -e "testing" | cracklib-check | grep -q 'dictionary'
 t_CheckExitStatus $?
 
-t_Log "checking simplistic password"
-echo -e "1234_Hgi" | cracklib-check | grep -q 'simplistic'
-t_CheckExitStatus $?
+if (t_GetPkgRel basesystem | grep -q el5)
+then
+  t_Log "Simplistic password not checked on C5"
+else
+  t_Log "checking simplistic password"
+  echo -e "1234_Hgi" | cracklib-check | grep -q 'simplistic'
+  t_CheckExitStatus $?
+fi
 
 t_Log "checking complicated password"
 echo -e "1536_Hargi" | cracklib-check | grep -q 'OK'
