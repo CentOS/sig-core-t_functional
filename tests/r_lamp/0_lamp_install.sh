38b41dd1809bdf237fdff691f5445c608229944f
Christoph Galuschka <christoph.galuschka@chello.at>
2012-06-10 14:48:09 +0200

    fix for httpd running sometimes in lamp test

--- tests/r_lamp/0_lamp_install.sh
+++ tests/r_lamp/0_lamp_install.sh
@@ -5,4 +5,12 @@
 t_Log "Running $0 - attempting to install LAMP stack."
 
 t_InstallPackage httpd mysql-server php
-t_ServiceControl httpd restart
+t_ServiceControl httpd stop
+ps ax | grep -v grep | grep -q httpd
+if [ $? = 0 ]
+then
+  t_Log "httpd still running - killing"
+  killall -9 httpd
+fi
+sleep 1
+t_ServiceControl httpd start
