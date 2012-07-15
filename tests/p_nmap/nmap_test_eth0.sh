8f72ef87fe26049b1edbc51e5f3154272891eee9
Athmane Madjoudj <athmane@fedoraproject.org>
2012-07-15 20:53:09 +0100

    Readd nmap test on eth0

---
+++ tests/p_nmap/nmap_test_eth0.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+
+t_Log "Running $0 - nmap querys eth0 and checks for open ssh-port"
+
+# Grabing IP of eth0
+IP=$(ip -f inet addr list eth0 | grep 'inet ')
+regex='.*inet\ (.*)\/.*'
+if [[ $IP =~ $regex ]]
+  then
+  t_Log "Found eth0 IP - starting nmap test"
+  nmap ${BASH_REMATCH[1]} | grep -iq ssh
+fi
+
+t_CheckExitStatus $?
