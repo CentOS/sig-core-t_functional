6ce9d31862dfd45285d51b52e83d8ac2ea95fc04
Sahil Muthoo <sahilm@thouhtworks.com>
2012-07-22 11:21:59 +0000

    Add ruby tests
    - verify ruby installed
    - verify irb installed
    - verify versions of both

---
+++ tests/0_ruby/0-install-ruby.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Author: White team
+
+t_Log "Running $0 - attempting to install openssl."
+t_InstallPackage ruby
+t_InstallPackage ruby-irb
+
+
---
+++ tests/0_ruby/10-ruby-installed-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: Nikhil Lanjewar <nikhil@lanjewar.com>
+# Author: Sahil Muthoo <sahilm@thoughtworks.com>
+# Author: Sahil Aggarwal <sahilagg@gmail.com>
+# Author: Saager Mhatre <saager.mhatre@gmail.com>
+
+t_Log "Running $0 - Check successful installation of ruby."
+ruby -v
+t_CheckExitStatus $?
+
