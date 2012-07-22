03d0b4b936668e42fb60a167e1ff9e20c8346d82
Sahil Muthoo <sahilm@thouhtworks.com>
2012-07-22 11:45:50 +0000

    Add more ruby tests
    - Successful ruby-ri, ruby-rdoc installation
    - Version check for ruby-ri
    - Version check for ruby-rdoc

--- tests/0_ruby/50-rdoc-test.sh
+++ tests/0_ruby/50-rdoc-test.sh
@@ -4,9 +4,9 @@
 # Author: Sahil Aggarwal <sahilagg@gmail.com>
 # Author: Saager Mhatre <saager.mhatre@gmail.com>
 
-t_Log "Running $0 - attempting to install ruby, ruby-irb, ruby-ri and ruby-rdoc4"
-t_InstallPackage ruby
-t_InstallPackage ruby-irb
-t_InstallPackage ruby-ri
-t_InstallPackage ruby-rdoc
+t_Log "Running $0 - Check successful installation of ruby rdoc."
+
+rdoc -v | grep 'V1.0.1'
+
+t_CheckExitStatus $?
 
