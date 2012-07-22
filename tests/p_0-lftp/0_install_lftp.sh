74f900b301c02913ff9461b72522e234885ad159
konarkm <modi.konark@gmail.com>
2012-07-22 12:26:25 +0000

    LFTP test updated

--- tests/p_0-lftp/0_install_lftp.sh
+++ tests/p_0-lftp/0_install_lftp.sh
@@ -1,2 +1,7 @@
+# Author: Piyush Kumar <piykumar@gmail.com>
+# Author: Munish Kumar <munishktotech@gmail.com>
+# Author: Ayush Gupta <ayush.001@gmail.com>
+# Author: Konark Modi <modi.konark@gmail.com>
+
 t_Log "Running $0 -installing lftp."
 t_InstallPackage lftp
