238ebc08c692715a8483e1b218e8b118056ea61f
Christoph Galuschka <christoph.galuschka@chello.at>
2012-07-27 23:21:04 +0200

    fix for file test to run on C5

--- tests/p_file/01_file_mime_application.sh
+++ tests/p_file/01_file_mime_application.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Author:	???
+#		Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - checking if file can recognixe mime executable type "
 
-file /bin/bash --mime-type | grep "application/x-executable"
+file /bin/bash -i | grep -q "application/x-executable"
 
 t_CheckExitStatus $?
