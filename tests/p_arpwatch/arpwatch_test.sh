89571c682ba5e071aa14e1bd534f7c741acb22cc
Christoph Galuschka <christoph.galuschka@chello.at>
2012-09-15 14:05:43 +0200

    changed p_arpwatch/arpwatch_test.sh: reenabled on C6 so we don't forget to fix the test changed p_lftp/10_lftp_http_test.sh: changed to run on CentOS sites and in qa-harness

--- tests/p_arpwatch/arpwatch_test.sh
+++ tests/p_arpwatch/arpwatch_test.sh
@@ -18,23 +18,17 @@ else
 fi
 
 # beginning and running test
-if [ $centos_ver == '6' ]
-  then
-  t_Log "Skipping for the time being on C6 until fixed"
-  ret_val=0
-else
-  arpwatch
-  sleep 4
-  arp -d $defgw
-  sleep 2
-  ping -q -i 0.5 -c 5 $defgw
-  killall arpwatch
-  sleep 2
-  grep -q $defgw $arpdat
-  ret_val=$?
-fi
+arpwatch
+sleep 4
+arp -d $defgw
+sleep 2
+ping -q -i 0.5 -c 5 $defgw
+killall arpwatch
+sleep 2
+grep -q $defgw $arpdat
+
+t_CheckExitStatus $?
 
 # cleaning up
 cat /dev/null > $arpdat
 
-t_CheckExitStatus $ret_val
