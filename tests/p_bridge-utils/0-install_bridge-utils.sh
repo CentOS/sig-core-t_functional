06ef83d26dad0ed18b38e929e3a7bbcad76e7c98
root <root@localhost.localdomain>
2012-09-15 10:12:58 -0700

    Added tests to install bridge-utils,add a bridge and delete a bridge

---
+++ tests/p_bridge-utils/0-install_bridge-utils.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Author: Madhurranjan Mohaan <mohanma@thoughtworks.com>
+
+# Install bridge_utils package
+t_Log "Running $0 - bridge-utils: Installation"
+
+t_InstallPackage bridge-utils
