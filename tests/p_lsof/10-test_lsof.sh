3764b59622d452b3bd43db9d6be870cf95c7463e
Madhurranjan Mohaan <madhurranjan.mohaan@gmail.com>
2012-09-16 11:51:54 -0700

    Removing the relative path dependency on installing ssh, instead calling it directly

--- tests/p_lsof/10-test_lsof.sh
+++ tests/p_lsof/10-test_lsof.sh
@@ -4,13 +4,9 @@
 
 t_Log "Running $0 - testing lsof against ssh port"
 
-../p_openssh/0-install_sshd.sh
+t_InstallPackage openssh-server openssh-clients 
+t_ServiceControl sshd start
 
-sshd_status=`service sshd status | grep running`
-if ! [ "$sshd_status" ]
-then
-  service sshd start
-fi
 sshd_port_listening=`lsof -i:22 | grep LISTEN`
 if [ "$sshd_port_listening" ]
 then
--- tests/p_telnet/10-test_telnet.sh
+++ tests/p_telnet/10-test_telnet.sh
@@ -4,13 +4,9 @@
 
 t_Log "Running $0 - testing telnet against ssh port"
 t_Log "Making sure port 22 is listening by calling ssh installer"
-../p_openssh/0-install_sshd.sh
 
-sshd_status=`service sshd status | grep running`
-if ! [ "$sshd_status" ]
-then
-  service sshd start
-fi
+t_InstallPackage openssh-server openssh-clients 
+t_ServiceControl sshd start
 
 telnet_port_22=`telnet 127.0.0.1 22 << EOH
 EOH`
