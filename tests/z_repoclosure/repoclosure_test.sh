8de2ed63debc87266de18a5a518495f529aac922
Athmane Madjoudj <athmane@fedoraproject.org>
2012-09-17 17:30:44 +0100

    Skip repoclosure test in non QA harness environment

--- tests/z_repoclosure/repoclosure_test.sh
+++ tests/z_repoclosure/repoclosure_test.sh
@@ -3,19 +3,24 @@
 
 t_Log "Running $0 -  repoclosure test ."
 
-t_InstallPackage yum-utils
+if [ $SKIP_QA_HARNESS ]; then
 
-t_Log "Running repoclosure test ..."
+    t_InstallPackage yum-utils
 
-repoclosure > /tmp/repoclosure.log 2>&1
-grep -q 'unresolved deps' /tmp/repoclosure.log
-if [ $? -eq 0 ] ; then
-    ret_val=1
+    t_Log "Running repoclosure test ..."
+
+    repoclosure > /tmp/repoclosure.log 2>&1
+    grep -q 'unresolved deps' /tmp/repoclosure.log
+    if [ $? -eq 0 ] ; then
+        ret_val=1
+    else
+        ret_val=0
+    fi
+
+    # print the output of repoclosure when it fails
+    [ $ret_val -eq 1 ] && cat /tmp/repoclosure.log
+
+    t_CheckExitStatus $ret_val
 else
-    ret_val=0
+    t_Log "Skip this test in non QA harness environment"
 fi
-
-# print the output of repoclosure when it fails
-[ $ret_val -eq 1 ] && cat /tmp/repoclosure.log
-
-t_CheckExitStatus $ret_val
