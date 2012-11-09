fe4cd38d830b6edacde3ea695476e94c34868927
Christoph Galuschka <christoph.galuschka@chello.at>
2012-11-09 17:06:11 +0100

    added tests for tracepath and mtr

---
+++ tests/p_iputils/tracepath_test.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+
+TEST=tracepath
+t_Log "Running $0 - running "${TEST}" to webhost"
+
+# Testing availability of network
+if [ $SKIP_QA_HARNESS -eq 1 ]; then
+  HOST="www.centos.org"
+else
+  HOST="repo.centos.qa"
+fi
+
+IP=$(ping -qn -c 1 ${HOST} | grep -i 'ping '${HOST})
+
+regex='PING\ '${HOST}'\ \(([0-9.]*)\).*'
+if [[ $IP =~ $regex ]]
+then
+  t_Log "resolved ${HOST} successfully - continuing"
+  ping -q -c 2 -i 0.25 ${HOST}
+  if [ $? = 0 ]
+  then
+    t_Log "$HOST is reachable - continuing"
+    COUNT=$( tracepath -n ${HOST} | grep -c ${BASH_REMATCH[1]} )
+    if [ $COUNT = 1 ]
+    then
+      t_Log "${TEST} reached ${HOST}"
+      ret_val=0
+    else
+      t_Log "${TEST} didn't reach ${HOST}"
+      ret_val=1
+    fi
+  fi
+else
+  t_Log "$HOST seems to be unavailable - skipping"
+  ret_val=0
+fi
+
+t_CheckExitStatus $ret_val
---
+++ tests/p_mtr/0-install_mtr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+
+t_Log "Running $0 -  installing package mtr"
+t_InstallPackage mtr
+
