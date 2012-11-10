ecc3193880b56ad141c0bf7a7e5071824fab346d
Christoph Galuschka <christoph.galuschka@chello.at>
2012-11-10 19:19:06 +0100

    fixed yum-plugin-fastestmirror to work on C5 - the rpm has a different name on C5

--- tests/p_yum-plugin-fastestmirror/0_install_yum-plugin-fastestmirror.sh
+++ tests/p_yum-plugin-fastestmirror/0_install_yum-plugin-fastestmirror.sh
@@ -2,9 +2,9 @@
 
 t_Log "Running $0 - package should already be installed"
 
-rpm -qa | grep yum-plugin-fastestmirror > /dev/null
+rpm -qa | grep yum | grep -q fastestmirror
 t_CheckExitStatus $?
 
 t_Log "Running $0 - yum should have a hard Requires on yum-plugin-fastestmirror"
-rpm -q --requires yum | grep yum-plugin-fastestmirror > /dev/null
+rpm -q --requires yum | grep yum | grep -q fastestmirror
 t_CheckExitStatus $?
