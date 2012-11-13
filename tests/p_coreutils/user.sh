bde4e7abb241f07d8b04e8ee9a5693b8faf4ed28
Christoph Galuschka <christoph.galuschka@chello.at>
2012-11-13 20:02:43 +0100

    made user.sh more verbose for debugging

--- tests/p_coreutils/user.sh
+++ tests/p_coreutils/user.sh
@@ -2,5 +2,16 @@
 # Author: Alice Kaerast <alice@kaerast.info>
 
 t_Log "$0 - test various user commands from coreutils"
+who
+echo $?
+who | grep -q "$LOGNAME" && who mom likes
+echo $?
+who | grep -q "$LOGNAME" && who mom likes | grep -q "$LOGNAME" && users
+echo $?
+who | grep -q "$LOGNAME" && who mom likes | grep -q "$LOGNAME" && users | grep -q "$LOGNAME" && id
+echo $?
+who | grep -q "$LOGNAME" && who mom likes | grep -q "$LOGNAME" && users | grep -q "$LOGNAME" && id | grep -q "$LOGNAME" && logname
+echo $?
 who | grep -q "$LOGNAME" && who mom likes | grep -q "$LOGNAME" && users | grep -q "$LOGNAME" && id | grep -q "$LOGNAME" && logname | grep -q "$LOGNAME"
+
 t_CheckExitStatus $?
