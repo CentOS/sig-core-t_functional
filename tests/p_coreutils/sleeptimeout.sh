bbf78c20b07d80eb000d7f65156c72c160f5a50c
Christoph Galuschka <christoph.galuschka@chello.at>
2012-11-13 19:36:26 +0100

    fixed sleeptimeout.sh to not run on C5 as there is no 'timeout' available.

--- tests/p_coreutils/sleeptimeout.sh
+++ tests/p_coreutils/sleeptimeout.sh
@@ -3,6 +3,14 @@
 
 t_Log "$0 checking timeout and sleep"
 
-timeout 1 sleep 2
-test $? -eq 124 && timeout 2 sleep 1
-t_CheckExitStatus $?
+if [ $centos_ver = 5 ]
+  then
+  t_Log "This is a C5 system. no 'timeout' available. Skipping." 
+  ret_val=0
+else
+  timeout 1 sleep 2
+  test $? -eq 124 && timeout 2 sleep 1
+  ret_val=$?
+fi
+
+t_CheckExitStatus $ret_val
