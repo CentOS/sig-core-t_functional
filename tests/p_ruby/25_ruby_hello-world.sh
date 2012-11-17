8b999b85e20d9c0992058455e0824c2b06d64e65
Christoph Galuschka <christoph.galuschka@chello.at>
2012-11-17 23:03:02 +0100

    fixed typo and added cleanup if testfile

--- tests/p_ruby/25_ruby_hello-world.sh
+++ tests/p_ruby/25_ruby_hello-world.sh
@@ -4,7 +4,7 @@
 t_Log "Running $0 - ruby can run 'hello world'"
 
 # create ruby script
-FILE=/var/tmp/test.sb
+FILE=/var/tmp/test.rb
 cat > $FILE <<EOF
 #!/usr/bin/ruby
 # Hello world ruby program
@@ -12,4 +12,8 @@ puts "hello world";
 EOF
 
 ruby $FILE | grep -q 'hello world'
+
 t_CheckExitStatus $?
+
+# and clean up
+/bin/rm ${FILE}
