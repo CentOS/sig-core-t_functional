549b5bf1340461546d6b446fbfee6bc49ebaa871
Christoph Galuschka <christoph.galuschka@chello.at>
2012-12-01 22:25:39 +0100

    added expect to installation suite as it is needed later in the tests

--- tests/p_gzip/0_install_gzip.sh
+++ tests/p_gzip/0_install_gzip.sh
@@ -2,5 +2,4 @@
 # Author: Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - attempting to install gzip."
-t_InstallPackage gzip ncompress zip diffutils less util-linux-ng
-
+t_InstallPackage gzip ncompress zip diffutils less util-linux-ng expect
