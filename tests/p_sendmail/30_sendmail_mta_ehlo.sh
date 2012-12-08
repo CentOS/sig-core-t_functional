2a3519d326563eb43ed64ec955b3df270d9342ad
Christoph Galuschka <christoph.galuschka@chello.at>
2012-12-08 12:00:49 +0100

    added test to sendmail to use ESMTP reordered tests

---
+++ tests/p_sendmail/30_sendmail_mta_ehlo.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+
+t_Log "Running $0 - sendmail can accept and deliver local email using ESMTP."
+
+MAILSPOOL=/var/spool/mail/root
+
+# make shure spool file is empty
+cat /dev/null > $MAILSPOOL
+ret_val=1
+
+# send mail to localhost
+echo -e "ehlo localhost\nmail from: root@localhost\nrcpt to: root@localhost\ndata\nt_functional test\n.\nquit\n" | nc -w 5 localhost 25 | grep -q "250 2.0.0"
+if [ $? = 0 ]
+  then
+  t_Log 'Mail has been queued successfully'
+  MTA_ACCEPT=0
+fi
+
+sleep 1
+grep -q 't_functional test' $MAILSPOOL
+if [ $? = 0 ]
+  then
+  t_Log 'previously sent mail is in '$MAILSPOOL
+  SPOOLFILE=0
+fi
+
+if ([ $MTA_ACCEPT = 0  ] && [ $SPOOLFILE = 0 ])
+  then
+  ret_val=0
+fi
+
+t_CheckExitStatus $ret_val
+
