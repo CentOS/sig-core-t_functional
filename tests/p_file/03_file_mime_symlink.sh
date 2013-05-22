571a6cc62ae129ecd1749f8fa223c86b7defdead
Athmane Madjoudj <athmane@fedoraproject.org>
2013-05-22 11:51:28 +0100

    Fix file symlink test

--- tests/p_file/03_file_mime_symlink.sh
+++ tests/p_file/03_file_mime_symlink.sh
@@ -4,12 +4,16 @@
 
 t_Log "Running $0 - checking if file can recognize symlink mime file type "
 
+TEST_FILE_PATH="/tmp/p_file_link_test"
+
+ln -s /etc/hosts $TEST_FILE_PATH
+
 if (t_GetPkgRel basesystem | grep -q el5)
   then
-  file -i /etc/grub.conf | grep -q 'x-not-regular-file'
+  file -i $TEST_FILE_PATH | grep -q 'x-not-regular-file'
   ret_val=$?
 else
-  file -i /etc/grub.conf | grep -q 'application/x-symlink'
+  file -i $TEST_FILE_PATH | grep -q 'application/x-symlink'
   ret_val=$?
 fi
 
