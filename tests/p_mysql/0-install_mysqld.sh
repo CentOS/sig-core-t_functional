81d9c84038d162da053b3da1cabc3527ae59b451
Christoph Galuschka <christoph.galuschka@chello.at>
2013-06-06 19:43:17 +0200

    added a log entry to p_mysql-server, reenabled the whole testsuite

--- tests/p_mysql/0-install_mysqld.sh
+++ tests/p_mysql/0-install_mysqld.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
+t_Log "Running $0 - installing and starting mysql server."
+
 # MySQL
 t_InstallPackage mysql-server
 t_ServiceControl mysqld start >/dev/null 2>&1
