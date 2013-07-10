a7ebfc4a4d474a8fef8bd7589e0e4b53d4fae103
Christoph Galuschka <christoph.galuschka@chello.at>
2013-07-10 20:58:02 +0200

    printing some more information on hostname variables for mysql connection test

--- tests/p_mysql/mysqld_conn_test.sh
+++ tests/p_mysql/mysqld_conn_test.sh
@@ -3,6 +3,9 @@
 t_Log "Running $0 - mysqld client can talk to mysql server."
 HostName=$(hostname -s)
 MySqlHostName=$(mysql -N -B -u root -e "show variables like 'hostname'" | cut -f 2 | cut -d . -f 1)
+# printing both variables for debugging purposes
+echo $HostName
+echo ${MySqlHostName}
 if [ $HostName = ${MySqlHostName} ]; then
 	ret_val=0
 else
