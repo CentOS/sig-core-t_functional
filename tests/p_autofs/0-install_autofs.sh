f15a7719375f91e352ef8dcf0e6cdb3b5161babd
Christoph Galuschka <christoph.galuschka@chello.at>
2013-09-29 12:31:23 +0200

    bug fixing to autofs test

--- tests/p_autofs/0-install_autofs.sh
+++ tests/p_autofs/0-install_autofs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Author: Christoph Galuschka <tigalch@tigalch.org>
 
-t_Log "Running $0 - Installing required packages"
+t_Log 'Running $0 - Installing required packages'
 t_InstallPackage autofs nfs-utils rpcbind
 
-t_Log "Preparing NFS-Share and starting NFS-Server"
-cat '/var/lib/ 127.0.0.1(ro)' > /etc/exports
+t_Log 'Preparing NFS-Share and starting NFS-Server'
+echo '/var/lib/ 127.0.0.1(ro)' >> /etc/exports
 t_ServiceControl rpcbind restart
 t_ServiceControl nfs restart
 
-t_Log "verify if NFs is mountable"
+t_Log 'verify if NFs is mountable'
 mount -t nfs 127.0.0.1:/var/lib /mnt
 ls -al /mnt | grep -q yum
 
--- tests/p_autofs/10-autofs-nfs-mount.sh
+++ tests/p_autofs/10-autofs-nfs-mount.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Author: Christoph Galuschka <tigalch@tigalch.org>
 
-t_Log "Running $0 - autofs can mount nfs share test."
+t_Log 'Running $0 - autofs can mount nfs share test.'
 
 t_Log 'Preparing autofs configuration'
 cp -a /etc/auto.master /etc/auto.master_orig
-cat '/autofs /etc/auto.autofs' >> /etc/auto.master
-cat 'nfs -fstype=nfs 127.0.0.1:/var/lib' > /etc/auto.autofs
+echo '/autofs /etc/auto.autofs' >> /etc/auto.master
+echo 'nfs -fstype=nfs 127.0.0.1:/var/lib' > /etc/auto.autofs
 
 t_ServiceControl autofs start
 
@@ -14,3 +14,8 @@ t_Log 'Running test'
 
 grep yum /autofs/nfs/
 #t_CheckExitStatus $ret_val
+
+# return everything to previous state
+cp -a /etc/auto.master_orig /etc/auto.master
+rm -rf /etc/auto.autofs
+cat /dev/null > /etc/exports
