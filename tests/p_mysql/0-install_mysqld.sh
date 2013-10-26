a410b0da6e0df557ca075db02c720aa96c8c5b6c
Christoph Galuschka <christoph.galuschka@chello.at>
2013-10-26 16:24:42 +0200

    due to 5.10, changed first mysql test to differ between mysql and mysql55

--- tests/p_mysql/0-install_mysqld.sh
+++ tests/p_mysql/0-install_mysqld.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+#	  Christoph Galuschka <tigalch@tigalch.org>
 
 t_Log "Running $0 - installing and starting mysql server."
 
 # MySQL
-t_InstallPackage mysql-server nc
-t_ServiceControl mysqld start >/dev/null 2>&1
+# starting with 5.10, we have to differ between mysql55 and mysql
+if [ $centos_ver = 5 ]
+then
+  t_InstallPackage mysql55-mysql-server nc
+  t_ServiceControl mysql55-mysqld start >/dev/null 2>&1
+else
+  t_InstallPackage mysql-server nc
+  t_ServiceControl mysqld start >/dev/null 2>&1
+fi
