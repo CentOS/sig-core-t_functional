7562181d8da8c53acc7998af24bab8bc6935aa33
Christoph Galuschka <christoph.galuschka@chello.at>
2013-12-03 18:57:07 +0100

    only define arch in lftp test of in qa-harness

--- tests/p_lftp/10_lftp_http_test.sh
+++ tests/p_lftp/10_lftp_http_test.sh
@@ -6,7 +6,6 @@
 # 	  Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - lftp: HTTP test"
-ARCH=$(t_GetArch)
 
 if [ $SKIP_QA_HARNESS -eq 1 ]; then
   CHECK_FOR="UTC"
@@ -14,6 +13,7 @@ if [ $SKIP_QA_HARNESS -eq 1 ]; then
   FILE="timestamp.txt"
 else
   CHECK_FOR="CentOS"
+  ARCH=$(t_GetArch)
   if [ $ARCH == "i686" ]; then
     URL="http://repo.centos.qa/srv/CentOS/6/os/i386/"
   else
