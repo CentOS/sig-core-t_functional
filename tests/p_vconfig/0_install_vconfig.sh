ff12d77741ec68d26bd3596d7b360e4073d62eb6
Christoph Galuschka <christoph.galuschka@chello.at>
2013-12-26 19:29:34 +0100

    changed vconfig test to only run on C5/C6

--- tests/p_vconfig/0_install_vconfig.sh
+++ tests/p_vconfig/0_install_vconfig.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Author: Christoph Galuschka <christoph.galuschka@chello.at>
+# Author: Christoph Galuschka <tigalch@tigalch.org>
 
-# Install vconfig
-t_InstallPackage vconfig
-
+if [ $centos_ver = 5 ] || [ $centos_ver = 6 ]
+then
+  # Install vconfig
+  t_InstallPackage vconfig
+else
+  t_Log 'This is only supported on C5 and C6, skipping'
+fi
