919c23ff1e2ec4dcfed3ec6f6156a74100f29304
Rene Diepstraten <rene@renediepstraten.nl>
2013-12-28 23:20:24 +0100

    busybox is not part of el7

--- tests/p_busybox/00_install_busybox.sh
+++ tests/p_busybox/00_install_busybox.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # Author: Christoph Galuschka <christoph.galuschka@chello.at>
+# Author: Rene Diepstraten <rene@renediepstraten.nl>
+
+[[ $centos_ver -ge 7 ]] && { t_Log "busybox is not part of el${centos_ver}" ; exit ; }
 
 t_Log "Running $0 - attempting to install busybox."
 t_InstallPackage busybox
