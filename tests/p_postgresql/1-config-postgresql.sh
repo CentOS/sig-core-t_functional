d7cea123862fc9354a4b4f4fa678db4db0b1c3b6
Borislav Andric <borislav.andric@gmail.com>
2013-12-30 10:39:06 +0100

    changed postgresql initdb to work on EL7

--- tests/p_postgresql/1-config-postgresql.sh
+++ tests/p_postgresql/1-config-postgresql.sh
@@ -3,13 +3,16 @@
 
 t_Log "Running $0 - initializing and starting PostgreSQL"
 
-if (t_GetPkgRel postgresql | grep -q el6)
-then
-   t_Log "Initialize PostgreSQL DB "     
+t_Log "Initialize PostgreSQL DB "
+if (t_GetPkgRel postgresql | grep -q el7) then
+   postgresql-setup initdb
+else if (t_GetPkgRel postgresql | grep -q el6) then
    service postgresql initdb
-   t_ServiceControl postgresql start
-   sleep 15
-else
-   t_ServiceControl postgresql start
-   sleep 15
-fi 
+fi
+fi
+
+t_ServiceControl postgresql start
+sleep 15
+
+
+ 
