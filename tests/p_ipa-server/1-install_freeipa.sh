b59b846997ef86afbf2eb3c1254ea0b7d878abeb
James Hogarth <james.hogarth@gmail.com>
2014-01-30 22:28:30 +0000

    make it a little noiser due to annyoing lack of feedback for a long while

--- tests/p_ipa-server/1-install_freeipa.sh
+++ tests/p_ipa-server/1-install_freeipa.sh
@@ -5,7 +5,7 @@
 if (t_GetPkgRel basesystem | grep -q el6)
 then
 t_Log "Running $0 - Installing packages"
-t_InstallPackage ipa-server bind-dyndb-ldap libsss_sudo &> /dev/null
+t_InstallPackage ipa-server bind-dyndb-ldap libsss_sudo 
 else
     echo "Skipped on CentOS 5"
 fi
--- tests/p_ipa-server/2-configure_freeipa.sh
+++ tests/p_ipa-server/2-configure_freeipa.sh
@@ -4,11 +4,13 @@
 if (t_GetPkgRel basesystem | grep -q el6)
 then
 
-t_Log "Configuring IPA server"
+t_Log "Running $0 - Configuring IPA server"
 
-ipa-server-install -U --hostname=c6test.c6ipa.local --ip-address=$(ip a s dev eth0 | awk '$0 ~ /scope global eth0/ {print $2}' | cut -d'/' -f 1) -r C6IPA.LOCAL -n c6ipa.local -p p455w0rd -a p455w0rd --ssh-trust-dns --setup-dns --forwarder=$(awk '$0 ~ /nameserver/ {print $2}' /etc/resolv.conf | head -n 1) &> /dev/null
+ipa-server-install -U --hostname=c6test.c6ipa.local --ip-address=$(ip a s dev eth0 | awk '$0 ~ /scope global eth0/ {print $2}' | cut -d'/' -f 1) -r C6IPA.LOCAL -n c6ipa.local -p p455w0rd -a p455w0rd --ssh-trust-dns --setup-dns --forwarder=$(awk '$0 ~ /nameserver/ {print $2}' /etc/resolv.conf | head -n 1) 
 
 t_CheckExitStatus $?
+
+t_Log "Running $0 - Enabling mkhomedir"
 authconfig --enablemkhomedir --enablesssd --update
 t_CheckExitStatus $?
 
