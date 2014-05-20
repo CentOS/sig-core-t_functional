b763226daa58f77659910ab21ac3c17c7a5ba6bb
Fabian Arrotin <arrfab@centos.org>
2014-05-20 13:20:50 +0200

    Changed nc options (nmap-nc) for el7/ipv4-ipv6 tests

--- tests/p_sendmail/10_sendmail_smtp.sh
+++ tests/p_sendmail/10_sendmail_smtp.sh
@@ -3,6 +3,6 @@
 
 t_Log "Running $0 - Sendmail SMTP test."
 
-echo "helo test" | nc -w 3 localhost 25 | grep -q '250'
+echo "helo test" | nc -4 -w 3 localhost 25 | grep -q '250'
 
 t_CheckExitStatus $?
