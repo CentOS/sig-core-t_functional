c3b6d5789dee585ab2c05ed3584c1355c62fdd0f
Fabian Arrotin <arrfab@centos.org>
2014-05-21 11:27:31 +0200

    Fixed the ipa-server test for el7

--- tests/p_ipa-server/0-preclean.sh
+++ tests/p_ipa-server/0-preclean.sh
@@ -3,52 +3,58 @@
 #
 
 if (t_GetPkgRel basesystem | grep -qE 'el(6|7)')
-then
+  then
 
-t_Log "Running $0 - Stopping and removing httpd if present"
-if  /sbin/service httpd status 2>&1 | grep 'running...' &> /dev/null
-then
-/sbin/service httpd stop &> /dev/null
-fi
-if rpm -q httpd &> /dev/null
-then
-/usr/bin/yum -y remove httpd &> /dev/null
-rm -rf /etc/httpd
-fi
+  t_Log "Running $0 - Stopping and removing httpd if present"
+  if  /sbin/service httpd status 2>&1 | grep 'running...' &> /dev/null
+    then
+    /sbin/service httpd stop &> /dev/null
+  fi
+  if rpm -q httpd &> /dev/null
+    then
+    /usr/bin/yum -y remove httpd &> /dev/null
+   rm -rf /etc/httpd
+  fi
 
-t_Log "Running $0 - Stopping and removing bind if present"
-if  /sbin/service named status 2>&1 | grep 'running...' &> /dev/null
-then
-/sbin/service named stop &> /dev/null
-fi
-if rpm -q bind &> /dev/null
-then
-/usr/bin/yum -y remove bind &> /dev/null
-rm -rf /etc/named /var/named
-fi
+  t_Log "Running $0 - Stopping and removing bind if present"
+  if  /sbin/service named status 2>&1 | grep 'running...' &> /dev/null
+    then
+    /sbin/service named stop &> /dev/null
+  fi
+  if rpm -q bind &> /dev/null
+    then
+    /usr/bin/yum -y remove bind &> /dev/null
+    rm -rf /etc/named /var/named
+  fi
 
 
-t_Log "Running $0 - Backing up resolv.conf"
-cp /etc/resolv.conf /tmp/resolv.conf.ipa-tests
+  t_Log "Running $0 - Backing up resolv.conf"
+  cp /etc/resolv.conf /tmp/resolv.conf.ipa-tests
 
-t_Log "Running $0 - Backing up nsswitch.conf"
-cp /etc/nsswitch.conf /tmp/nsswitch.conf.ipa-tests
+  t_Log "Running $0 - Backing up nsswitch.conf"
+  cp /etc/nsswitch.conf /tmp/nsswitch.conf.ipa-tests
 
-t_Log "Running $0 - Backing up hosts file"
-cp /etc/hosts /tmp/hosts.ipa-tests
+  t_Log "Running $0 - Backing up hosts file"
+  cp /etc/hosts /tmp/hosts.ipa-tests
 
-if [[ -f /etc/ntp.conf ]]
-then
-t_Log "Running $0 - Backing up ntp.conf file"
-cp /etc/ntp.conf /tmp/ntp.conf.ipa-tests
-fi
+  t_Log "Running $0 - Saving current hostname"
+  echo $(hostname) >/tmp/hostname.ipa-tests
 
-t_Log "Running $0 - Backing up saving yum history id"
-/usr/bin/yum history list | awk 'NR == 4 {print $1}' > /tmp/yum-rollback-id.ipa-tests
+  if [[ -f /etc/ntp.conf ]]
+    then
+    t_Log "Running $0 - Backing up ntp.conf file"
+    cp /etc/ntp.conf /tmp/ntp.conf.ipa-tests
+  fi
 
-t_Log "Running $0 - Cleaning up hosts file"
-sed -i "s/127.0.0.1 $(hostname)//" /etc/hosts
+  t_Log "Running $0 - Backing up saving yum history id"
+  /usr/bin/yum history list | awk 'NR == 4 {print $1}' > /tmp/yum-rollback-id.ipa-tests
 
+  t_Log "Running $0 - Cleaning up hosts file"
+  sed -i "s/127.0.0.1 $(hostname)//" /etc/hosts
+  
+  t_Log "Running $0 - Cleaning previous tomcat/pki instances if any"
+  yum remove -d0 -y tomcat ipa-server >/dev/null 2>&1
+  /bin/rm -Rf /var/lib/pki/pki-tomcat/ /etc/sysconfig/pki-tomcat /var/log/pki/pki-tomcat /etc/pki/pki-tomcat /etc/sysconfig/pki/tomcat/pki-tomcat
 
 else
     echo "Skipped on CentOS 5"
--- tests/p_ipa-server/2-configure_freeipa.sh
+++ tests/p_ipa-server/2-configure_freeipa.sh
@@ -2,31 +2,38 @@
 # Author: James Hogarth <james.hogarth@gmail.com>
 #
 if (t_GetPkgRel basesystem | grep -qE 'el(6|7)')
-then
+  then
 
-t_Log "Running $0 - setting hostname of system"
-hostname c6test.c6ipa.local
-echo "$(ip a s dev eth0 | awk '$0 ~ /scope global eth0/ {print $2}' | cut -d'/' -f 1) $(hostname)" >> /etc/hosts
-hostname | grep "c6test.c6ipa.local" &> /dev/null
-t_CheckExitStatus $?
+  t_Log "Running $0 - setting hostname of system"
+  eth_int=$(ip addr|grep -B 1 "link/ether"|head -n 1|awk '{print $2}'|tr -d ':')
+  eth_ip=$(ip -4 addr show dev $eth_int | awk '$0 ~ /scope global/ {print $2}' | cut -d'/' -f 1)
+  if [ "$centos_ver" = "7" ] ; then
+    hostnamectl set-hostname c6test.c6ipa.local 
+  else
+    hostname c6test.c6ipa.local
+  fi
+
+  echo $eth_ip $(hostname) >> /etc/hosts
+  hostname | grep "c6test.c6ipa.local" &> /dev/null
+  t_CheckExitStatus $?
 
 
-t_Log "Running $0 - Configuring IPA server - this can take some time"
+  t_Log "Running $0 - Configuring IPA server - this can take some time"
 
-ipa-server-install -U --hostname=c6test.c6ipa.local --ip-address=$(ip a s dev eth0 | awk '$0 ~ /scope global eth0/ {print $2}' | cut -d'/' -f 1) -r C6IPA.LOCAL -n c6ipa.local -p p455w0rd -a p455w0rd --ssh-trust-dns --setup-dns --forwarder=$(awk '$0 ~ /nameserver/ {print $2}' /etc/resolv.conf | head -n 1) 
+  ipa-server-install -U --hostname=c6test.c6ipa.local --ip-address=$eth_ip -r C6IPA.LOCAL -n c6ipa.local -p p455w0rd -a p455w0rd --ssh-trust-dns --setup-dns --forwarder=$(awk '$0 ~ /nameserver/ {print $2}' /etc/resolv.conf | head -n 1) 
 
-t_CheckExitStatus $?
+  t_CheckExitStatus $?
 
-t_Log "Running $0 - Enabling mkhomedir"
-authconfig --enablemkhomedir --enablesssd --update
-t_CheckExitStatus $?
+  t_Log "Running $0 - Enabling mkhomedir"
+  authconfig --enablemkhomedir --enablesssd --update
+  t_CheckExitStatus $?
 
-if /sbin/service sssd status | grep 'is stopped' &> /dev/null
-then
-/sbin/service sssd start &> /dev/null
-fi
+  if /sbin/service sssd status | grep 'is stopped' &> /dev/null
+    then
+    /sbin/service sssd start &> /dev/null
+  fi
 
 else
-    echo "Skipped on CentOS 5"
+  echo "Skipped on CentOS 5"
 fi
 
