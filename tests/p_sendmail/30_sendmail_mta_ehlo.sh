c57e2420570a40e3978e7f951734423ac6fa5daa
Fabian Arrotin <arrfab@centos.org>
2014-05-21 08:14:08 +0200

    Change to the p_sendmail test for el7

--- tests/p_sendmail/30_sendmail_mta_ehlo.sh
+++ tests/p_sendmail/30_sendmail_mta_ehlo.sh
@@ -1,31 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 # Author: Christoph Galuschka <christoph.galuschka@chello.at>
 
 t_Log "Running $0 - sendmail can accept and deliver local email using ESMTP."
 
-MAILSPOOL=/var/spool/mail/root
+MAILSPOOL="/var/spool/mail/root"
 
 # make shure spool file is empty
-cat /dev/null > $MAILSPOOL
+cat /dev/null > ${MAILSPOOL}
 ret_val=1
 
 # send mail to localhost
 echo -e "ehlo localhost\nmail from: root@localhost\nrcpt to: root@localhost\ndata\nt_functional test\n.\nquit\n" | nc -4 -w 5 localhost 25 | grep -q "250 2.0.0"
-if [ $? = 0 ]
+if [ "$?" = "0" ]
   then
   t_Log 'Mail has been queued successfully'
-  MTA_ACCEPT=0
+  MTA_ACCEPT="0"
 fi
 
-sleep 1
-grep -q 't_functional test' $MAILSPOOL
-if [ $? = 0 ]
+sleep 3
+grep -q 't_functional test' ${MAILSPOOL}
+if [ "$?" = "0" ]
   then
-  t_Log 'previously sent mail is in '$MAILSPOOL
-  SPOOLFILE=0
+  t_Log "previously sent mail is in ${MAILSPOOL}"
+  SPOOLFILE="0"
 fi
 
-if [ "$MTA_ACCEPT" = "0"  ] && [ "$SPOOLFILE" = "0" ] ; then
+if [[ "$MTA_ACCEPT" = "0" &&  "$SPOOLFILE" = "0" ]] ; then
   ret_val=0
 fi
 
