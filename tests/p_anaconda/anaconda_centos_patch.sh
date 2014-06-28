87eada103e47a71cf36a069e86a5955fd1550438
Fabian Arrotin <arrfab@centos.org>
2014-06-28 22:31:35 +0200

    Fixed anaconda file to check now that we have our own centos.py installclasses file on C7

--- tests/p_anaconda/anaconda_centos_patch.sh
+++ tests/p_anaconda/anaconda_centos_patch.sh
@@ -5,13 +5,15 @@ t_Log "Running $0 - CentOS Anaconda patch is applied test."
 
 if [ "$centos_ver" = "7" ];then
   ANACONDA_PATH=/usr/lib64/python2.7/site-packages/pyanaconda/
+  ANACONDA_FILE="centos.py"
 else
   ANACONDA_PATH=/usr/lib/anaconda/
+  ANACONDA_FILE="rhel.py"
 fi
 
-(grep "id = \"centos\"" $ANACONDA_PATH/installclasses/rhel.py >/dev/null 2>&1) &&\
+(grep "id = \"centos\"" $ANACONDA_PATH/installclasses/$ANACONDA_FILE >/dev/null 2>&1) &&\
 
-(grep "name = N_(\"CentOS Linux\")" $ANACONDA_PATH/installclasses/rhel.py >/dev/null 2>&1) 
+(grep "name = N_(\"CentOS Linux\")" $ANACONDA_PATH/installclasses/$ANACONDA_FILE >/dev/null 2>&1) 
 
 
 t_CheckExitStatus $?
