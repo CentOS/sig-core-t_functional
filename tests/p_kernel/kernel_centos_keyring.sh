0c76e10fa1c1556690e144125c284f3c017d2353
Fabian Arrotin <arrfab@centos.org>
2014-06-28 23:41:44 +0200

    Fixed the kernel test to check for x.509 certs on c7 kernel

--- tests/p_kernel/kernel_centos_keyring.sh
+++ tests/p_kernel/kernel_centos_keyring.sh
@@ -4,6 +4,15 @@
 
 t_Log "Running $0 -  check CentOS' Kernel Module GPG key."
 
-grep 'User ID: CentOS (Kernel Module GPG key)' /var/log/dmesg > /dev/null 2>&1
+if [ "$centos_ver" = "7" ] ; then
+  for id in kpatch "Driver Update" kernel
+  do
+    t_Log "Verifying x.509 CentOS ${id}"
+    grep 'CentOS Linux ${id} signing key' /var/log/dmesg > /dev/null 2>&1
+    t_CheckExitStatus $?
+  done
+else
+  grep 'User ID: CentOS (Kernel Module GPG key)' /var/log/dmesg > /dev/null 2>&1
+  t_CheckExitStatus $?
+fi
 
-t_CheckExitStatus $?
