9c593782b508fd092c7e7376aab2313d393da170
Christoph Galuschka <christoph.galuschka@chello.at>
2014-06-29 18:38:32 +0200

    added test for conversion of test to postscript to PDF and back

---
+++ tests/r_pdf/00_install_requirements.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Christoph Galuschka <tigalch@tigalch.org>
+
+t_Log "Running $0 -  install package enscript, ghostscript and pdftotext"
+t_InstallPackage enscript ghostscript poppler-utils
+
