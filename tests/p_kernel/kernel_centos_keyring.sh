1ced04532fb4ff53a765f3320c907c77668cbe74
Fabian Arrotin <arrfab@centos.org>
2014-07-01 15:33:44 +0200

    Fixed the case sensitive grep operation to find the kernel x.509 cert in the c7 kernel

--- tests/p_kernel/kernel_centos_keyring.sh
+++ tests/p_kernel/kernel_centos_keyring.sh
@@ -5,10 +5,10 @@
 t_Log "Running $0 -  check CentOS' Kernel Module GPG key."
 
 if [ "$centos_ver" = "7" ] ; then
-  for id in kpatch "Driver Update" kernel
+  for id in kpatch "Driver update" kernel
   do
     t_Log "Verifying x.509 CentOS ${id}"
-    grep 'CentOS Linux ${id} signing key' /var/log/dmesg > /dev/null 2>&1
+    grep -i "CentOS Linux ${id} signing key" /var/log/dmesg > /dev/null 2>&1
     t_CheckExitStatus $?
   done
 else
