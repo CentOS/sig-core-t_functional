a09abc0e6263bfad304e434c5666beed4b881410
Christoph Galuschka <tigalch@tigalch.org>
2014-07-07 22:52:32 +0200

    test now queries timestamp.txt in and outside of QA-harness. no longer testing ffor release notes, as the tree for C7 no longer contains this file.

--- tests/p_lftp/10_lftp_http_test.sh
+++ tests/p_lftp/10_lftp_http_test.sh
@@ -3,24 +3,18 @@
 # Author: Munish Kumar <munishktotech@gmail.com>
 # Author: Ayush Gupta <ayush.001@gmail.com>
 # Author: Konark Modi <modi.konark@gmail.com>
-# 	  Christoph Galuschka <christoph.galuschka@chello.at>
+# 	  Christoph Galuschka <tigalch@tigalch.org>
 
 t_Log "Running $0 - lftp: HTTP test"
 
 if [ $SKIP_QA_HARNESS -eq 1 ]; then
-  CHECK_FOR="UTC"
   URL="http://mirror.centos.org/"
-  FILE="timestamp.txt"
 else
-  CHECK_FOR="CentOS"
-  ARCH=$(t_GetArch)
-  if [ $ARCH == "i686" ]; then
-    URL="http://repo.centos.qa/srv/CentOS/6/os/i386/"
-  else
-    URL="http://repo.centos.qa/srv/CentOS/6/os/x86_64/"
-  fi
-  FILE="RELEASE-NOTES-en-US.html"
+  URL="http://repo.centos.qa/srv/CentOS/"
 fi
+
+CHECK_FOR="UTC"
+FILE="timestamp.txt"
 
 t_Log "Querying ${URL}${FILE}"
 
