0a1924579388345df72909152cf9bc8cda524d91
Christoph Galuschka <tigalch@tigalch.org>
2014-09-27 20:01:25 +0200

    added resourcing of functions library to reflect CVE-2014-6271

--- tests/0_common/20_upgrade_all.sh
+++ tests/0_common/20_upgrade_all.sh
@@ -3,5 +3,10 @@
 t_Log "Running $0 - test that all updates can be applied to this machine cleanly"
 
 yum -d0 -y upgrade
+ret_val=$?
 
-t_CheckExitStatus $?
+t_Log "Resourcing lib-functions - see CVE-2014-6271"
+LIB_FUNCTIONS='./tests/0_lib/functions.sh'
+source $LIB_FUNCTIONS
+
+t_CheckExitStatus $ret_val
