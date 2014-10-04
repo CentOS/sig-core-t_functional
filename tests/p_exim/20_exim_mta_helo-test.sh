18c0db8de4e618d25809056521047ee0b1b9f5c6
Christoph Galuschka <tigalch@tigalch.org>
2014-10-04 17:47:39 +0200

    fixed bugs/typos

--- tests/p_exim/20_exim_mta_helo-test.sh
+++ tests/p_exim/20_exim_mta_helo-test.sh
@@ -4,6 +4,7 @@
 t_Log "Running $0 - exim can accept and deliver local email."
 
 if [ $centos_ver == '5' ]
+  then
   ret_val=1
 
   # send mail to localhost
@@ -13,8 +14,6 @@ if [ $centos_ver == '5' ]
     then
     t_Log 'Mail has been queued successfully'
   fi
-
-250 OK id=1XaR7Q-0005D8-Cm
 
   regex='250\ OK\ id\=([0-9A-Za-z-]*)'
   if [[ $mail =~ $regex ]]
@@ -28,7 +27,7 @@ if [ $centos_ver == '5' ]
     ret_val=0
   fi
 else
-  t_Log t_Log "This is not a C5 system - skipping"
+  t_Log "This is not a C5 system - skipping"
   ret_val=0
 fi
 
