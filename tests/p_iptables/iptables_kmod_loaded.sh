48f3e22204333f9dc3ef38816af836cc918807bf
Christoph Galuschka <tigalch@tigalch.org>
2014-11-07 13:09:26 +0100

    fixed test: C7 uses firewalld

--- tests/p_iptables/iptables_kmod_loaded.sh
+++ tests/p_iptables/iptables_kmod_loaded.sh
@@ -3,6 +3,12 @@
 
 t_Log "Running $0 - check if iptables kernel modules are loaded"
 
+if [ "$centos_ver" = "7" ];then
+ t_Log "CentOS $centos_ver uses firewalld and not iptables -> SKIP"
+ t_CheckExitStatus 0
+ exit 0
+fi
+
 lsmod | grep "ip_tables" > /dev/null 2>&1
 
 t_CheckExitStatus $?
