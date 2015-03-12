295ce62d49bbbe7450c23c74a1d26ba30598631a
Christoph Galuschka <tigalch@tigalch.org>
2015-03-12 21:45:23 +0100

    expanded p_abrt to run on C7, added Matej to authors file

--- tests/p_abrt/abrt_gpg_keys_test.sh
+++ tests/p_abrt/abrt_gpg_keys_test.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
+#	  Christoph Galuschka <tigalch@tigalch.org>
 # Note: This was a known issue in CentOS 6.0
 # See: http://bugs.centos.org/view.php?id=4993
+# Christoph Galuschka: added functionality for C7
 
 t_Log "Running $0 -  check that abrt is using CentOS' gpg keys."
 
-if (t_GetPkgRel basesystem | grep -q el6)
-then
+if [ "$centos_ver" = "7" ] ;then
+    ls /etc/pki/rpm-gpg/ | grep -q "RPM-GPG-KEY-CentOS"
+elif [ "$centos_ver" = "6" ] ; then
     grep -q "RPM-GPG-KEY-CentOS" /etc/abrt/gpg_keys
 else 
     echo "Skipped on CentOS 5"
