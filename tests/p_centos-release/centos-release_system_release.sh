6747bde8b63753662a1d22be48091f7b968642f0
Fabian Arrotin <arrfab@centos.org>
2015-03-23 18:17:36 +0100

    Modified the script as now redhat-release content is different on 7 1503

--- tests/p_centos-release/centos-release_system_release.sh
+++ tests/p_centos-release/centos-release_system_release.sh
@@ -2,10 +2,12 @@
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
 t_Log "Running $0 - /etc/centos-release has correct branding"
-if (t_GetPkgRel basesystem | grep -q el6)
-then
-    grep "CentOS" /etc/centos-release >/dev/null 2>&1
-else
+
+if [ "$centos_ver" = "5" ] ; then
     grep "CentOS" /etc/redhat-release >/dev/null 2>&1
+else
+    grep "CentOS" /etc/centos-release >/dev/null 2>&1
 fi
+
 t_CheckExitStatus $?
+
