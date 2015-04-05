e019d08550e1fd398efdc74de41d4ebcc52ed75d
Christoph Galuschka <tigalch@tigalch.org>
2015-04-05 13:32:54 +0200

    merged install commands into one line

--- tests/p_tcpdump/00_install_tcpdump.sh
+++ tests/p_tcpdump/00_install_tcpdump.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Author: Christoph Galuschka <tigalch@tigalch.org>
 
-t_Log "Running $0 -  install package tcpdump"
-t_InstallPackage tcpdump
-t_InstallPackage psmisc
+t_Log "Running $0 -  install package tcpdump and psmisc"
+t_InstallPackage tcpdump psmisc
 
