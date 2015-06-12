df26007afb54893dcafe6a87a7a6134e89a07b89
Christoph Galuschka <tigalch@tigalch.org>
2015-06-12 19:10:06 +0200

    added yum list to show all available/used packages for the test

--- tests/0_common/20_upgrade_all.sh
+++ tests/0_common/20_upgrade_all.sh
@@ -4,4 +4,8 @@ t_Log "Running $0 - test that all updates can be applied to this machine cleanly
 
 yum -d0 -y upgrade
 
+t_Log "Running $0 - listing all used/available packages"
+
+yum list
+
 t_CheckExitStatus $?
