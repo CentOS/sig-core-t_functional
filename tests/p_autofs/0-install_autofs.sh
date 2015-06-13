a7355181177ca1a6ea52111119707036600fb748
Fabian Arrotin <arrfab@centos.org>
2015-06-13 16:58:45 +0200

    Modified the p_autofs test for c5 when installed with @core

--- tests/p_autofs/0-install_autofs.sh
+++ tests/p_autofs/0-install_autofs.sh
@@ -2,12 +2,25 @@
 # Author: Christoph Galuschka <tigalch@tigalch.org>
 
 t_Log "Running $0 - Installing required packages"
-t_InstallPackage autofs nfs-utils rpcbind
+
+if [ "$centos_ver" = "5" ] ; then
+   t_InstallPackage autofs nfs-utils portmap
+else
+   t_InstallPackage autofs nfs-utils rpcbind 
+fi
+
 
 t_Log 'Preparing NFS-Share and starting NFS-Server'
 echo '/var/lib/ 127.0.0.1(ro)' >> /etc/exports
-t_ServiceControl rpcbind restart
-t_ServiceControl nfs restart
+
+if [ "$centos_ver" = "5" ] ; then
+   t_ServiceControl portmap restart
+   t_ServiceControl nfs restart
+else
+   t_ServiceControl rpcbind restart
+   t_ServiceControl nfs restart
+fi
+
 
 t_Log 'verify if NFS is mountable'
 mount -t nfs 127.0.0.1:/var/lib /mnt
