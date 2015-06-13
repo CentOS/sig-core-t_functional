40873514a2c1077fdf78074b1d948d6a9ea14f91
Athmane Madjoudj <athmane@fedoraproject.org>
2015-06-13 18:16:47 +0100

    Add @fonts to the install requirements

--- tests/r_pdf/00_install_requirements.sh
+++ tests/r_pdf/00_install_requirements.sh
@@ -2,5 +2,5 @@
 # Author: Christoph Galuschka <tigalch@tigalch.org>
 
 t_Log "Running $0 -  install package enscript, ghostscript and pdftotext"
-t_InstallPackage enscript ghostscript poppler-utils
+t_InstallPackage enscript ghostscript poppler-utils @fonts
 
