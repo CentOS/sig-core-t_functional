62013cfda86b687e50d6dd9486a01b369b80cb5d
Athmane Madjoudj <athmane@fedoraproject.org>
2015-06-13 20:38:17 +0100

    Add a workarround for pdf test

--- tests/r_pdf/00_install_requirements.sh
+++ tests/r_pdf/00_install_requirements.sh
@@ -2,5 +2,10 @@
 # Author: Christoph Galuschka <tigalch@tigalch.org>
 
 t_Log "Running $0 -  install package enscript, ghostscript and pdftotext"
-t_InstallPackage enscript ghostscript poppler-utils @fonts
+
+# Workarround for post scriptlet non-fatal errors
+yum -y remove bitstream-vera* liberation*
+#
+t_InstallPackage fontconfig @fonts
+t_InstallPackage enscript ghostscript poppler-utils
 
