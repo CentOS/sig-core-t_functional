6a55f1f4a030ff7bf2c80127053b1840f3fabcd9
Christoph Galuschka <tigalch@tigalch.org>
2015-06-17 22:15:59 +0200

    preinstalling java-1.6 to fix current java issues with ipa-server, to be fixed with 6.7

--- tests/p_ipa-server/1-install_freeipa.sh
+++ tests/p_ipa-server/1-install_freeipa.sh
@@ -4,6 +4,8 @@
 
 if (t_GetPkgRel basesystem | grep -qE 'el(6|7)') && !(t_GetArch | grep -qE 'aarch64')
   then
+  t_Log "Running $0 - Installing java-1.6 to fix java-issues with 1.7/1.8, to be fixed with 6.7"
+  t_InstallPackage java-1.6.0-openjdk
   t_Log "Running $0 - Installing packages, this takes around 2-3 mins"
   t_InstallPackage ipa-server bind-dyndb-ldap libsss_sudo
 else
