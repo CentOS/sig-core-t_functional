a592652c788f3ad90e852094a6ad7c84fd5e2ca6
Fabian Arrotin <arrfab@centos.org>
2015-09-21 09:38:14 +0200

    Switched to string search for grub2/shim cert validation

--- tests/p_grub2/01_grub2_secureboot_signed.sh
+++ tests/p_grub2/01_grub2_secureboot_signed.sh
@@ -5,7 +5,7 @@ t_Log "Running $0 -  Verifying that grub2-efi is correctly signed with correct c
 
 if [ "$centos_ver" = "7" ] ; then
   t_InstallPackage pesign grub2-efi
-  pesign --show-signature --in /boot/efi/EFI/centos/grubx64.efi|grep -q 0x7f55a3434808
+  pesign --show-signature --in /boot/efi/EFI/centos/grubx64.efi|grep -q 'Red Hat Inc.'
   t_CheckExitStatus 0
 else
   t_log "previous versions than CentOS 7 aren't using secureboot ... skipping"
--- tests/p_shim/01_shim_secureboot_signed.sh
+++ tests/p_shim/01_shim_secureboot_signed.sh
@@ -5,7 +5,7 @@ t_Log "Running $0 -  Verifying that shim.efi is correctly signed with correct ce
 
 if [ "$centos_ver" = "7" ] ; then
   t_InstallPackage pesign shim
-  pesign --show-signature --in /boot/efi/EFI/centos/shim.efi|grep -q 0x7f7ff2a0f1e0
+  pesign --show-signature --in /boot/efi/EFI/centos/shim.efi|grep -q 'Red Hat Inc.'
   t_CheckExitStatus 0
 else
   t_log "previous versions than CentOS 7 aren't using shim/secureboot ... skipping"
