57d60bd2a63fd18298196b9a2a51711699bf0387
Johnny Hughes <johnny@centos.org>
2015-10-20 08:43:35 -0500

    modify the ixgbe test to look specifically for the extra kmod version and not the version included in the kernel

--- tests/p_ixgbe/01_kmod-ixgbe.sh
+++ tests/p_ixgbe/01_kmod-ixgbe.sh
@@ -4,8 +4,7 @@ t_Log "Running $0 -  Verifying that kmod-ixbe installs and loads"
 
 if [ "$centos_ver" = "7" ] ; then
   t_InstallPackage kmod-ixgbe
-  modprobe ixgbe
-  lsmod | grep -q 'ixgbe'
+  modnfo ixgbe | grep -q 'filename' | grep -q 'extra' | grep -q 'ixgbe.ko'
   t_CheckExitStatus $?
 else
   t_log "previous versions than CentOS 7 aren't using kmod-ixgbe ... skipping"
