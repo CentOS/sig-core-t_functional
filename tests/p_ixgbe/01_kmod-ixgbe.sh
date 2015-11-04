3a4e81f468c76f597cfd70eb5aa24271798e781b
Johnny Hughes <johnny@centos.org>
2015-11-04 08:36:36 -0600

    fix ixgbe test for newer kernel

--- tests/p_ixgbe/01_kmod-ixgbe.sh
+++ tests/p_ixgbe/01_kmod-ixgbe.sh
@@ -4,7 +4,7 @@ t_Log "Running $0 -  Verifying that kmod-ixbe installs and loads"
 
 if [ "$centos_ver" = "7" ] ; then
   t_InstallPackage kmod-ixgbe
-  modinfo ixgbe | grep 'filename' | grep 'extra' | grep -q 'ixgbe.ko'
+  modinfo ixgbe | grep 'filename' | egrep 'extra|weak' | grep -q 'ixgbe.ko'
   t_CheckExitStatus $?
 else
   t_log "previous versions than CentOS 7 aren't using kmod-ixgbe ... skipping"
