225533a6c18a973a3ec2d7afbd8fbb1b4464f398
Christoph Galuschka <tigalch@tigalch.org>
2016-02-06 23:56:00 +0100

    added test to check for www.centos.org as startpage in the firefox default settings

---
+++ tests/p_firefox/00-install_firefox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Christoph Galuschka <tigalch@tigalch.org>
+
+# Install firefox
+
+t_Log "Running $0 - installation of firefox."
+
+t_InstallPackage firefox
+
---
+++ tests/p_firefox/10-check_default_startpage.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Author: Christoph Galuschka <tigalch@tigalch.org>
+
+# Check for centos.org in preferences.js
+t_Log "Running $0 - firefox has www.centos.org as default page."
+
+if [ $(t_GetArch) = i686 ]
+  then
+  path='/usr/lib/firefox/defaults/preferences/all-redhat.js'
+  else
+  path='/usr/lib64/firefox/defaults/preferences/all-redhat.js'
+fi
+
+count=$(grep -c www.centos.org $path)
+
+if [ $count=2 ]
+  then
+  t_CheckExitStatus 0
+  else
+  t_CheckExitStatus 1
+fi
