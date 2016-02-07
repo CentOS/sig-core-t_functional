0ae3356095cad27d331344a56952df04462d5f5a
Christoph Galuschka <tigalch@tigalch.org>
2016-02-07 14:49:21 +0100

    small fix re t_GetArch variable

--- tests/p_firefox/10-check_default_startpage.sh
+++ tests/p_firefox/10-check_default_startpage.sh
@@ -4,11 +4,11 @@
 # Check for centos.org in preferences.js
 t_Log "Running $0 - firefox has www.centos.org as default page."
 
-if [ $(t_GetArch) = i686 ]
+if (t_GetArch firefox | grep -q 'x86_64')
   then
-  path='/usr/lib/firefox/defaults/preferences/all-redhat.js'
-  else
   path='/usr/lib64/firefox/defaults/preferences/all-redhat.js'
+  else
+  path='/usr/lib/firefox/defaults/preferences/all-redhat.js'
 fi
 
 count=$(grep -c www.centos.org $path)
