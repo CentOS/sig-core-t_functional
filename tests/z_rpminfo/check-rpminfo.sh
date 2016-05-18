0b568f26fdbfa12bf703a28c8a43d8630205628e
Brian Stinson <brian@bstinson.com>
2016-05-18 08:36:22 -0500

    Add some rpminfo checks and start on some overrides for existing pkgs
    We need to check the vendor and packager attributes before packages are
    released, so this script runs against the preupdates repo (if running in
    preupdates). It also will run against the base repos if run outside
    preupdates.

---
+++ tests/z_rpminfo/check-rpminfo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: Brian Stinson <brian@bstinson.com>
+# Runner script for checking rpminfo so we can fail gracefully on EL5
+
+if [[ $centos_ver != 5 ]] 
+then
+    python "$(dirname "$(readlink -f "$0")")/check-rpminfo.py"
+else
+    echo "Skipped on CentOS 5"
+fi
