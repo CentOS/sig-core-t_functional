a78f8087170d7220cfd5789a474682af361a017a
Johnny Hughes <johnny@centos.org>
2016-05-18 10:47:44 -0500

    Revert "t_CheckExitStatus to have optional error message"

--- tests/p_vconfig/10_test_vconfig.sh
+++ tests/p_vconfig/10_test_vconfig.sh
@@ -15,22 +15,44 @@ fi
 # create VLAN-IF 10 on eth0
 vconfig add eth0 10
 ip addr list | grep -q eth0.10
-t_CheckExitStatus $? "VLAN-IF creation failed"
-t_Log "VLAN-IF successfully created"
+if [ $? == 1 ]
+  then
+  t_Log "VLAN-IF creation failed"
+  ret_val=1
+else
+  t_Log "VLAN-IF successfully created"
+fi
 
 #assign IP address on VLAN-IF
 ifconfig eth0.10 172.16.30.1 netmask 255.255.255.255
 ip addr list | grep -q 172.16.30.1
-t_CheckExitStatus $? "IP address assignment on eth0.10 failed"
-t_Log "IP address successfully assigned on eth0.10"
+if [ $? == 1 ]
+  then
+  t_Log "IP address assignment on eth0.10 failed"
+  ret_val=1
+else
+  t_Log "IP address successfully assigned on eth0.10"
+fi
 
 #testing address with ping
 ping -c 4 -q 172.16.30.1 | grep -q '4 received'
-t_CheckExitStatus $? "pinging on eth0.10 failed"
-t_Log "local ping on VLAN IF worked"
+if [ $? == 1 ]
+  then
+  t_Log "pinging on eth0.10 failed"
+  ret_val=1
+else
+  t_Log "local ping on VLAN IF worked"
+fi
 
 # delete VLAN-IF 10 on eth0
 vconfig rem eth0.10
 ip addr list | grep -q eth0.10
-t_CheckExitStatus $? "Removing VLAN IF failed"
-t_Log "Removing of VLAN IF worked"
+if [ $? == 0 ]
+  then
+  t_Log "Removing VLAN IF failed"
+  ret_val=1
+else
+  t_Log "Removing of VLAN IF worked"
+fi
+
+t_CheckExitStatus $ret_val
