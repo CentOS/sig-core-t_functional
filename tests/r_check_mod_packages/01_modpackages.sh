2c674cd6802709a85556671c83944103e532d66a
Johnny Hughes <johnny@centos.org>
2016-08-23 07:05:11 -0500

    add pcs to c7 modified packages check

--- tests/r_check_mod_packages/01_modpackages.sh
+++ tests/r_check_mod_packages/01_modpackages.sh
@@ -11,7 +11,7 @@ else
    
    t_Log "Running $0 - Checking current repositories for .el7.centos on modified files"
    yum clean all
-    for pkg in PackageKit abrt apache-commons-net anaconda basesystem centos-indexhtml centos-logos centos-release chrony compat-glibc curl dhcp firefox glusterfs grub2 httpd initial-setup ipa-client kabi-yum-plugins kde-settings libreport ntp oscap-anaconda-addon openssl098e plymouth redhat-rpm-config redhat-lsb scap-security-guide shim-unsigned sos subscription-manager system-config-date system-config-kdump thunderbird xulrunner yum 
+    for pkg in PackageKit abrt apache-commons-net anaconda basesystem centos-indexhtml centos-logos centos-release chrony compat-glibc curl dhcp firefox glusterfs grub2 httpd initial-setup ipa-client kabi-yum-plugins kde-settings libreport ntp oscap-anaconda-addon openssl098e pcs plymouth redhat-rpm-config redhat-lsb scap-security-guide shim-unsigned sos subscription-manager system-config-date system-config-kdump thunderbird xulrunner yum 
       do 
          has_centos=$(yum list $pkg | grep '.el7.centos')
          if [ "$has_centos" == "" ]; then 
@@ -20,7 +20,7 @@ else
          fi
       done
   else
-    t_Log "This test only for CentOS-7, skipping Modified Packages test ..."
+    t_Log "Skipping $0 - This test only for CentOS-7, skipping Modified Packages test ..."
     ret_val=0
   fi
 
