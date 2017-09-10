91a50d73c0b761d0f7f2cefa32ea0bf851a0c362
Athmane Madjoudj <athmane@fedoraproject.org>
2017-09-11 01:31:04 +0200

    Adapt losetup since it does not support bulk detach on c6

--- tests/p_lvm2/99-lvm-tests-cleanup.sh
+++ tests/p_lvm2/99-lvm-tests-cleanup.sh
@@ -9,13 +9,14 @@ disk1=`losetup -a | grep t_lvm_temp_dir/disk1.img | cut -d: -f1`
 disk2=`losetup -a | grep t_lvm_temp_dir/disk2.img | cut -d: -f1`
 disk3=`losetup -a | grep t_lvm_temp_dir/disk3.img | cut -d: -f1`
 disk4=`losetup -a | grep t_lvm_temp_dir/disk4.img | cut -d: -f1`
+all_disks="$disk0 $disk1 $disk2 $disk3 $disk4"
 
 lvremove -f /dev/vg_t_lvm/lv_t_lvm_test && \
 lvremove -f /dev/vg_t_lvm_mirror/lv_mirror_test && \
 vgremove -f vg_t_lvm && \
 vgremove -f vg_t_lvm_mirror && \
-pvremove -fy $disk0 $disk1 $disk2 $disk3 $disk4 &&\
-losetup -D
+pvremove -fy $all_disks &&\
+(for disk in $all_disks ; do losetup -d $disk ; done)
 
 
 t_CheckExitStatus $?
