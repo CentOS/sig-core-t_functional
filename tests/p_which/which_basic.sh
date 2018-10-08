ccb05afe550e007b70e1e631eb86f4bcef18cfca
Alex Baranowski <alex@euro-linux.com>
2018-10-08 16:36:03 +0200

    Fix which_basic.sh test

--- tests/p_which/which_basic.sh
+++ tests/p_which/which_basic.sh
@@ -2,5 +2,7 @@
 # Author: Alice Kaerast <alice@kaerast.info>
 
 t_Log "Running $0 - Checking which can find itself"
-/usr/bin/which which | grep -q "/usr/bin/which"
+# which package provides /usr/bin/which and /bin/which 
+# so results are depending on PATH variable
+/usr/bin/which which | grep -E -q "/usr/bin/which|/bin/which"
 t_CheckExitStatus $?
