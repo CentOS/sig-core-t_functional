d950e85e922e3f8bc702676f7449617653795f80
Pablo Greco <pablo@fliagreco.com.ar>
2019-01-02 07:55:20 -0300

    Enable lsb tests for armhfp

--- tests/p_lsb/0-install_redhat-lsb.sh
+++ tests/p_lsb/0-install_redhat-lsb.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
-uname_arch=$(uname -m)
-
-if [ "$uname_arch" == "armv7l" ]; then
-  t_Log "*** Not testing on Arch: $uname_arch ***"
-  exit 0
-fi
-
 t_Log "$0 - Installing redhat-lsb"
 t_InstallPackage  redhat-lsb
-
--- tests/p_lsb/lsb_release_brand_test.sh
+++ tests/p_lsb/lsb_release_brand_test.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
-t_Log "Running $0 - LSB CentOS braning check."
-
-uname_arch=$(uname -m)
-
-if [ "$uname_arch" == "armv7l" ]; then
-  t_Log "*** Not testing on Arch: $uname_arch ***"
-  exit 0
-fi
+t_Log "Running $0 - LSB CentOS branding check."
 
 lsb_release -i | grep -q "CentOS" && \
-lsb_release -d | grep -q "CentOS"  
+lsb_release -d | grep -q "CentOS"
 
 t_CheckExitStatus $?
