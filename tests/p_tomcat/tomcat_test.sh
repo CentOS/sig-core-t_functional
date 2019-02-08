5a7c8e41f3e50bc4178bd816382761112017a32a
Johnny Hughes <johnny@centos.org>
2019-02-08 09:09:51 -0600

    adjusting tomcat_test,sh to skip i386

--- tests/p_tomcat/tomcat_test.sh
+++ tests/p_tomcat/tomcat_test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
+uname_arch=$(uname -m)
+if [ "$uname_arch" == "i686" ] ; then
+t_Log "Skipping $0 on $uname_arch"
+exit 0
+else
 t_Log "Running $0 - Tomcat basic test."
+fi
 
 if [ "$centos_ver" = "7" ] ; then
  string_tosearch="you've successfully installed Tomcat. Congratulations!"
