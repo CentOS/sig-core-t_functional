55a1f3e6a4732a21c0b38f5227df8674b92f8f51
Johnny Hughes <johnny@centos.org>
2019-03-22 12:40:10 -0500

    install deltarpm before upgrade

--- tests/0_common/20_upgrade_all.sh
+++ tests/0_common/20_upgrade_all.sh
@@ -2,6 +2,7 @@
 
 t_Log "Running $0 - test that all updates can be applied to this machine cleanly"
 
+yum -d0 -y install deltarpm
 yum -d0 -y upgrade
 
 t_Log "Running $0 - listing all used/available packages"
