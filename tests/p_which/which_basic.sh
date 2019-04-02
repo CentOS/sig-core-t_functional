7c4feb7a6413f6e84e5258d9bc15e39672bd789c
Pablo Greco <pablo@fliagreco.com.ar>
2019-04-02 14:47:14 -0300

    Fix typo

--- tests/p_which/which_basic.sh
+++ tests/p_which/which_basic.sh
@@ -2,5 +2,5 @@
 # Author: Alice Kaerast <alice@kaerast.info>
 
 t_Log "Running $0 - Checking which can find itself"
-/usr/bin/which which | grep -e -q '^(/usr)?/bin/which$'
+/usr/bin/which which | grep -E -q '^(/usr)?/bin/which$'
 t_CheckExitStatus $?
