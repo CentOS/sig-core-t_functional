4732f927978782c89c512175264fd467e98bc49a
Alex Baranowski <alex@euro-linux.com>
2019-05-07 11:57:50 +0200

    Fix shim tests for machines that uses legacy boot

--- tests/p_shim/01_shim_secureboot_signed.sh
+++ tests/p_shim/01_shim_secureboot_signed.sh
@@ -4,6 +4,12 @@
 t_Log "Running $0 -  Verifying that shim.efi is correctly signed with correct cert"
 
 if [[ "$centos_ver" = "7" && "$arch" = "x86_64" ]] ; then
+  [ -e /boot/efi/EFI/redhat/shim.efi ] && BOOT_EFI="/boot/efi/EFI/redhat/shim.efi"
+  [ -e /boot/efi/EFI/centos/shim.efi ] && BOOT_EFI="/boot/efi/EFI/centos/shim.efi"
+  if [ -z "$BOOT_EFI" ]; then 
+      t_Log "WARNING: Cannot find proper shim.efi ! This host probably uses legacy boot :(."
+      exit 0
+  fi
   t_InstallPackage pesign shim
   pesign --show-signature --in /boot/efi/EFI/centos/shim.efi|grep -q 'Microsoft Windows UEFI Driver Publisher'
   t_CheckExitStatus $?
