f21da5c13bdad62eecc4bfb7a90c3e7eba066191
Pablo Greco <pgreco@centosproject.org>
2019-07-27 19:45:12 -0300

    Fix selinux tests in CentOS8

--- tests/p_selinux/0_install_tools.sh
+++ tests/p_selinux/0_install_tools.sh
@@ -4,6 +4,8 @@
 t_Log "Running $0 -  install package selinux policycoreutils tools"
 if [ "$centos_ver" = "6" ] ; then
   t_InstallPackage policycoreutils-python
+elif [ "$centos_ver" = "8" ] ; then
+  t_InstallPackage python3-libselinux
 else
   t_InstallPackage libselinux-python
 fi
--- tests/p_selinux/selinux_enforce.sh
+++ tests/p_selinux/selinux_enforce.sh
@@ -3,14 +3,14 @@
 
 t_Log "Running $0 - check if SELinux is in enforcing mode"
 
-if [ "$centos_ver" = "7" ] ; then
+if [ "$centos_ver" -ge 7 ] ; then
   selinux_file=/sys/fs/selinux/enforce
 else
   selinux_file=/selinux/enforce
 fi
 
 if [ "$SKIP_QA_HARNESS" = "1" ] ; then
-    echo "Skipping this test ..."
+    t_Log "Skipping this test ..."
 else
     cat $selinux_file | grep 1  > /dev/null 2>&1
     t_CheckExitStatus $?
