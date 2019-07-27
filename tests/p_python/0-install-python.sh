aee7846ab513c74932a32885f7f9d78b17e74a66
Pablo Greco <pgreco@centosproject.org>
2019-07-27 14:41:13 -0300

    Fix Python test in CentOS8

--- tests/p_python/0-install-python.sh
+++ tests/p_python/0-install-python.sh
@@ -4,4 +4,8 @@
 # Install python
 t_Log "Running $0 - installing python."
 
+if [ "$centos_ver" -ge 8 ] ; then
+t_InstallPackage python36
+else
 t_InstallPackage python
+fi
--- tests/p_python/20-python-mysql-test.sh
+++ tests/p_python/20-python-mysql-test.sh
@@ -4,10 +4,15 @@
 
 t_Log "Running $0 - python can retrieve mysql-server version information."
 
+if [ "$centos_ver" -ge 8 ] ; then
+PYTHON=python3
+else
+PYTHON=python
+fi
 # we need a working and running mysql server
 # starting with 5.10, we have to differ between mysql55 and mysql
 
-if [ "$centos_ver" = "7" ] ; then
+if [ "$centos_ver" -ge 7 ] ; then
   my_packages="mariadb mariadb-server nc"
   mysql_service="mariadb"
 elif [ "$centos_ver" = "5" ] ;then
@@ -21,28 +26,34 @@ fi
 t_ServiceControl ${mysql_service} start >/dev/null 2>&1
 
 # Installing additional python/mysql module
+if [ "$centos_ver" -ge 8 ] ; then
+t_InstallPackage python3-PyMySQL
+importcomponent="pymysql"
+else
 t_InstallPackage MySQL-python
+importcomponent="MySQLdb"
+fi
 
 # create python Scrip
 SCRIPT='/var/tmp/test.py'
 
 cat >$SCRIPT <<EOF
-import MySQLdb
+import $importcomponent
 
-conn = MySQLdb.connect (host = "localhost",
+conn = $importcomponent.connect (host = "localhost",
                            user = "",
                            passwd = "",
-                           db = "test")
+                           db = "")
 cursor = conn.cursor ()
 cursor.execute ("SELECT VERSION()")
 row = cursor.fetchone ()
-print "server version:", row[0]
+print ("server version:", row[0])
 cursor.close ()
 conn.close ()
 EOF
 
 # If we execute the script and get the version it works
-python $SCRIPT |grep -q 'server version'
+$PYTHON $SCRIPT |grep -q 'server version'
 
 t_CheckExitStatus $?
 
