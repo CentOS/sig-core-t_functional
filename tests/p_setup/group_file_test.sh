2a3685416c319a1c6b5a39a86de7ff313cbdcebd
Fabian Arrotin <arrfab@centos.org>
2019-07-27 18:11:13 +0200

    Fixed p_setup test for c8
    Signed-off-by: Fabian Arrotin <arrfab@centos.org>

--- tests/p_setup/group_file_test.sh
+++ tests/p_setup/group_file_test.sh
@@ -3,10 +3,16 @@
 
 t_Log "Running $0 - group file sanity check."
 
+if [ "$centos_ver" -eq "8" ] ;then
+  nobody_grpid="65534"
+else
+  nobody_grpid="99"
+fi
+
 grep -q 'root:x:0' /etc/group && \
 grep -q 'bin:x:1' /etc/group && \
 grep -q 'daemon:x:2' /etc/group && \
-grep -q 'nobody:x:99' /etc/group 
+grep -q "nobody:x:${nobody_grpid}" /etc/group 
 
 
 t_CheckExitStatus $?
--- tests/p_setup/passwd_file_test.sh
+++ tests/p_setup/passwd_file_test.sh
@@ -3,8 +3,14 @@
 
 t_Log "Running $0 - passwd file sanity check."
 
+if [ "$centos_ver" -eq "8" ] ;then
+  nobody_grpid="65534"
+else
+  nobody_grpid="99"
+fi
+
 grep -q 'root:x:0' /etc/passwd && \
-grep -q 'nobody:x:99' /etc/passwd
+grep -q "nobody:x:${nobody_grpid}" /etc/passwd
 
 
 t_CheckExitStatus $?
