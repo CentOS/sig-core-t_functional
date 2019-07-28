a44872cea650bc91f90bf93fbdf473429a4797f9
Pablo Greco <pgreco@centosproject.org>
2019-07-28 16:12:49 -0300

    Skip some yum tests in CentOS8

--- tests/p_yum/yum_bugtracker.sh
+++ tests/p_yum/yum_bugtracker.sh
@@ -3,6 +3,10 @@
 
 t_Log "Running $0 - Yum is using CentOS' bugtracker test."
 
+if [ "$centos_ver" -ge "8" ] ; then
+   t_Log "CentOS$ver, SKIP"
+   exit 0
+fi
 grep "http://bugs.centos.org" /etc/yum.conf >/dev/null 2>&1
 
 t_CheckExitStatus $?
