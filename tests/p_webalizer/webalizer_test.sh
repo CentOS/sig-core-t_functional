a544776260519aaee59434a6935ec255ab132053
Pablo Greco <pgreco@centosproject.org>
2019-07-28 16:05:26 -0300

    Webalizer not available in CentOS8, disable test

--- tests/p_webalizer/webalizer_test.sh
+++ tests/p_webalizer/webalizer_test.sh
@@ -3,7 +3,7 @@
 
 t_Log "Running $0 - Webalizer test."
 
-if [ "$centos_ver" = "7" ] ; then
+if [ "$centos_ver" -ge 7 ] ; then
  t_Log "No webalizer package for CentOS $centos_ver -> SKIP"
  t_CheckExitStatus 0
  exit 0
