95afe3b3cc821ea7f26a6c19fb9277493d59b45a
Pablo Greco <pgreco@centosproject.org>
2019-07-28 16:30:47 -0300

    Fix spamassassin tests in CentOS8

--- tests/p_spamassassin/spamassassin_test.sh
+++ tests/p_spamassassin/spamassassin_test.sh
@@ -3,8 +3,10 @@
 
 t_Log "Running $0 - check that spamassassin can detect spam."
 
-VERSION=`t_GetPkgVer spamassassin`
+if [ "$centos_ver" -lt 8 ] ; then
+VERSION=-`t_GetPkgVer spamassassin`
+fi
 spamassassin --lint &&\
-spamassassin < /usr/share/doc/spamassassin-${VERSION}/sample-spam.txt | grep -q 'X-Spam-Flag: YES'
+spamassassin < /usr/share/doc/spamassassin${VERSION}/sample-spam.txt | grep -q 'X-Spam-Flag: YES'
 
 t_CheckExitStatus $?
