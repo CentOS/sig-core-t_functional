cbca3e7911cf74a29a8bda27f5c292150e64e358
Fabian Arrotin <arrfab@centos.org>
2019-07-29 19:35:13 +0200

    Fixed systemd test with unit/service that is common on el7/el8
    Signed-off-by: Fabian Arrotin <arrfab@centos.org>

--- tests/p_systemd/15-systemctl_list_non-native-services.sh
+++ tests/p_systemd/15-systemctl_list_non-native-services.sh
@@ -5,8 +5,6 @@
 [ ${centos_ver} -lt 7 ] && exit
 t_Log "Running $0 - Checking if systemctl can check if a non-native service is enabled"
 
-# netconsole is used as example because it's a non native service with minimal install
-
-systemctl is-enabled netconsole.service 2> /dev/null | grep -q -E 'enabled|disabled'
+systemctl is-enabled kdump.service 2> /dev/null | grep -q -E 'enabled|disabled'
 
 t_CheckExitStatus $?
