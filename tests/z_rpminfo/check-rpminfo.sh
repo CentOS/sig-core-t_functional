59d8ad8e4ab430bf8d82772d2661fcc24b3f508c
Fabian Arrotin <arrfab@centos.org>
2019-07-29 23:24:32 +0200

    Disabled .py rpminfo script on c8 - not python3 ready
    Signed-off-by: Fabian Arrotin <arrfab@centos.org>

--- tests/z_rpminfo/check-rpminfo.sh
+++ tests/z_rpminfo/check-rpminfo.sh
@@ -2,6 +2,11 @@
 # Author: Brian Stinson <brian@bstinson.com>
 # Runner script for checking rpminfo so we can fail gracefully on EL5
 
+if [ "$centos_ver" -eq "8" ]; then
+  t_Log "check-rpminfo.py not python3/dnf ready on c8 => SKIP"
+  exit 0
+fi
+
 if [[ $centos_ver != 5 ]] 
 then
     python "$(dirname "$(readlink -f "$0")")/check-rpminfo.py"
