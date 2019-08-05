18e6d41bed9cc8b70d45490a07079e32942b5989
Fabian Arrotin <arrfab@centos.org>
2019-08-05 22:57:22 +0200

    Disabled lynx test on c8 as pkg not in base repo
    Signed-off-by: Fabian Arrotin <arrfab@centos.org>

--- tests/p_lynx/lynx_default_page_centos_test.sh
+++ tests/p_lynx/lynx_default_page_centos_test.sh
@@ -3,6 +3,12 @@
 
 t_Log "Running $0 - check that lynx default page is CentOS welcome page ."
 
+if [ "$centos_ver" -eq "8" ]; then
+  t_Log "Package lynx not available in default repos on c8 => SKIP"
+  exit 0
+fi
+
+
 lynx -dump  | grep "Welcome to CentOS"  >/dev/null 2>&1
 
 t_CheckExitStatus $?
--- tests/p_lynx/lynx_dump_page_test.sh
+++ tests/p_lynx/lynx_dump_page_test.sh
@@ -3,6 +3,12 @@
 
 t_Log "Running $0 - check that lynx can dump remote page."
 
+if [ "$centos_ver" -eq "8" ]; then
+  t_Log "Package lynx not available in default repos on c8 => SKIP"
+  exit 0
+fi
+
+
 if [ "$SKIP_QA_HARNESS" = "1" ] ; then
   URL="http://mirror.centos.org/"
   CHECK_FOR="CentOS on the Web"
