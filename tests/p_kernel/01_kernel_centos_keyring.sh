8c2386de4025c838a5808a4327dac0be901cc607
Johnny Hughes <johnny@centos.org>
2019-09-12 14:33:11 -0500

    fix 01_kernel_centos_keyring.sh for power9

--- tests/p_kernel/01_kernel_centos_keyring.sh
+++ tests/p_kernel/01_kernel_centos_keyring.sh
@@ -5,6 +5,9 @@
 t_Log "Running $0 -  check CentOS' Kernel Module GPG key."
 
 uname_arch=$(uname -m)
+uname_kernel=$(uname -r)
+uname_kernel=${uname_kernel:0:6}
+
 
 if [ "$uname_arch" == "aarch64" ] || [ "$uname_arch" == "armv7l" ] || [ "$uname_arch" == "i686" ]; then
   t_Log "*** Not testing on Arch: $uname_arch ***"
@@ -13,7 +16,12 @@ fi
 
 if [ "$centos_ver" -ge 7 ] ; then
   if [ "$centos_ver" -eq 7 ];then
-    ring=.system_keyring
+    if [ "$uname_arch" == "ppc64le" -a "$uname_kernel" == "4.18.0" ];then
+      # power9 with c8 kernel
+      ring=.builtin_trusted_keys
+    else
+      ring=.system_keyring
+    fi
   else
     ring=.builtin_trusted_keys
   fi
