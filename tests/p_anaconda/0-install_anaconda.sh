c2e4f98712cb1df2077ffa1891b1a9f3b98b68db
Johnny Hughes <johnny@centos.org>
2020-06-25 15:06:19 -0500

    do not do the anaconda test in c8

--- tests/p_anaconda/0-install_anaconda.sh
+++ tests/p_anaconda/0-install_anaconda.sh
@@ -2,6 +2,10 @@
 # Author: Athmane Madjoudj <athmanem@gmail.com>
 
 uname_arch=$(uname -m)
+if [ "$centos_ver" -eq "8" ]; then
+  t_Log "c8 => SKIPPING"
+  exit 0
+fi
 
 if [ "$uname_arch" == "armv7l" ]; then
   t_Log "*** Not testing on Arch: $uname_arch ***"
--- tests/p_anaconda/anaconda_centos_patch.sh
+++ tests/p_anaconda/anaconda_centos_patch.sh
@@ -4,6 +4,10 @@
 t_Log "Running $0 - CentOS Anaconda patch is applied test."
 
 uname_arch=$(uname -m)
+if [ "$centos_ver" -eq "8" ]; then
+  t_Log "c8 => SKIPPING"
+  exit 0
+fi
 
 if [ "$uname_arch" == "aarch64" ] || [ "$uname_arch" == "i686" ] || [ "$uname_arch" == "armv7l" ]; then
   t_Log "*** Not testing on Arch: $uname_arch ***"
@@ -12,9 +16,6 @@ fi
 
 if [ "$centos_ver" = "7" ];then
   ANACONDA_PATH=/usr/lib64/python2.7/site-packages/pyanaconda/
-  ANACONDA_FILE="centos.py"
-elif [ "$centos_ver" = "8" ];then
-  ANACONDA_PATH=/usr/lib64/python3.6/site-packages/pyanaconda/
   ANACONDA_FILE="centos.py"
 else
   ANACONDA_PATH=/usr/lib/anaconda/
