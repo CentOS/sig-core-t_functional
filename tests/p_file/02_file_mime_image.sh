d7bd9fb871d02c5bc741d8ef4401161f3a736365
Alex Iribarren <Alex.Iribarren@cern.ch>
2020-07-23 11:59:40 +0200

    Look for any png file

--- tests/p_file/02_file_mime_image.sh
+++ tests/p_file/02_file_mime_image.sh
@@ -5,14 +5,7 @@
 
 t_Log "Running $0 - checking if file can recognize image mime file type "
 
-case $centos_ver in
-  5|6)
-    pngfile=/usr/lib/anaconda-runtime/boot/syslinux-splash.png
-    ;;
-  *)
-    pngfile=/usr/share/anaconda/boot/syslinux-splash.png
-    ;;
-esac
+pngfile="$(find /usr/share/ -name '*.png' -print -quit)"
 
 file $pngfile -i | grep -q 'image/png'
 
