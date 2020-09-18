01b93d24c24bc7a52335c6fc4385aa4ea4e0dad2
Brian Stinson <bstinson@centosproject.org>
2020-09-18 15:19:39 -0500

    Look in the CentOS-Base or CentOS-Stream-BaseOS repo files for proper ID

--- tests/p_centos-release/centos-release_centos-base_repos.sh
+++ tests/p_centos-release/centos-release_centos-base_repos.sh
@@ -3,6 +3,6 @@
 
 t_Log "Running $0 - CentOS Base repos sanity test."
 
-grep "name=CentOS" /etc/yum.repos.d/CentOS-Base.repo >/dev/null 2>&1
+grep "name=CentOS" /etc/yum.repos.d/CentOS*-Base*.repo >/dev/null 2>&1
 
 t_CheckExitStatus $?
