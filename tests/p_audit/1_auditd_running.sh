9d8d96c23f4d8a87921454e599216bc5d46b443e
Adam Saleh <adam@asaleh.net>
2022-03-16 09:49:58 +0000

    Adding env-var flag to skip tests not runnable in containers.

--- tests/p_audit/1_auditd_running.sh
+++ tests/p_audit/1_auditd_running.sh
@@ -3,7 +3,7 @@
 
 t_Log "Running $0 -  check if auditd is running."
 
-if [ $SKIP_QA_HARNESS -eq 1 ]; then
+if [ $SKIP_QA_HARNESS -eq 1 -o $CONTAINERTEST -eq 1 ]; then
     echo "Skipping this test ..."
 else
     service auditd status > /dev/null 2>&1
--- tests/p_audit/3_auditd_logging.sh
+++ tests/p_audit/3_auditd_logging.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # Author: Athmane Madjoudj <athmanem@gmail.com>
-
+if [ $SKIP_QA_HARNESS -eq 1 -o $CONTAINERTEST -eq 1 ]; then
+	    echo "Skipping this test ..."
+else
 t_Log "Running $0 -  check if audit log is not empty."
 
 [[ -s /var/log/audit/audit.log ]] 
 
 t_CheckExitStatus $?
+fi
