cdaebbd43152e0f14f74ed713714336de6e0bb24
Johnny Hughes <johnny@centos.org>
2022-10-04 11:57:36 -0500

    Fix p_libbpf to run on EL7

--- tests/p_libbpf/0_install_libbpf.sh
+++ tests/p_libbpf/0_install_libbpf.sh
@@ -4,6 +4,9 @@
 # Author: Konark Modi <modi.konark@gmail.com>
 
 t_Log "Running $0 -installing libbpf and bcc-tools."
-t_InstallPackage libbpf
-t_InstallPackage bcc-tools
-
+if [ "$centos_ver" -ge 8 ] ; then
+ t_InstallPackage libbpf
+ t_InstallPackage bcc-tools
+else
+ t_log "Skip on less than EL8"
+fi
--- tests/p_libbpf/10_test_bcc_tools.sh
+++ tests/p_libbpf/10_test_bcc_tools.sh
@@ -8,7 +8,7 @@
 
 t_Log "Running $0 - bcc-tools: argdist test"
 
-if [ -z "$CONTAINERTEST" ] ; then
+if [ -z "$CONTAINERTEST" ] && [ "$centos_ver" -ge 8 ]; then
     /usr/share/bcc/tools/argdist -i 1 -n 5 -C 'p:c:umask(u32 mask):u32:mask'
 else
     t_Log "Running in container -> SKIP"
