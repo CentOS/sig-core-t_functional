4baccd9ece758786c9ca74cc7f7ab735d59784a6
Carlos Rodriguez-Fernandez <carlosrodrifernandez@gmail.com>
2023-08-17 11:07:17 -0700

    sysstat:fix iostat, mpstat tests
    Fixes: #91
    Signed-off-by: Carlos Rodriguez-Fernandez <carlosrodrifernandez@gmail.com>

--- tests/p_sysstat/00-install_sysstat.sh
+++ tests/p_sysstat/00-install_sysstat.sh
@@ -3,3 +3,9 @@
 
 # SysStat Performance Monitoring Tools for Linux
 t_InstallPackage sysstat
+
+if [ "$centos_ver" -lt 8 ] ; then
+  t_InstallPackage psmisc
+fi
+
+
--- tests/p_sysstat/10-iostat-disk-basic.sh
+++ tests/p_sysstat/10-iostat-disk-basic.sh
@@ -5,40 +5,41 @@
 
 t_Log "Running $0 - a basic iostat test to verify disk measurement"
 
-# Save our iostat output somewhere
-TMP=/tmp/iostat.disk.scratch
+output_file=$(mktemp)
+trap "rm -f ${output_file}" EXIT
 
 # dd options
-BS=4196
-COUNT=10100
-SUM=$(expr $BS \* $COUNT / 1024)
+bs=4196
+count=10100
+sum=$(expr $bs \* $count / 1024)
 
-# Clean up after ourselves
-trap "[ -e $TMP ] && { /bin/rm -f $TMP; }" EXIT
 
 # Clear out the pagecache to get an accurate reading
 echo 1 > /proc/sys/vm/drop_caches
 
 # Capture a storage device name
-DRIVE=$(fdisk -l|grep -Po -m1 '^/dev/[\D]+')
+drive=$(fdisk -l|grep -Po -m1 '^/dev/[\D]+')
 
 # Run iostat on the device
-/usr/bin/iostat -dkx 1 5 $DRIVE >$TMP &
+/usr/bin/iostat -d 1 5 $drive >${output_file} 2>&1 &
 
-# Let the dust settle
-sleep 4
+# Time for iostat booting
+sleep 1
 
 # Generate some read traffic
-/bin/dd if=$DRIVE of=/dev/null bs=$BS count=$COUNT &>/dev/null
+/bin/dd if=$drive of=/dev/null bs=$bs count=$count &>/dev/null
 
 # Give iostat a chance to log our traffic
 sleep 6
 
 # Confirm our read bytes are >0, excluding the first 
 # line since that's the average since boot.
-BYTES_READ=$(awk '$6 ~ /[0-9]/ {NR>1 && sum+=$6} END {print int(sum)}' $TMP)
+kbytes_read=$(awk '$6 ~ /[0-9]/ {NR>1 && sum+=$6} END {print int(sum)}' ${output_file})
 
-# Check we read at least as much as requested
-[ "$BYTES_READ" -ge "$SUM" ] || { t_Log "iostat didn't log as much traffic as we generated?!...that ain't good"; }
+if [ "$kbytes_read" -eq 0 ]; then
+  t_Log "FAIL: ${0}: no io activity registered"
+  cat ${output_file}
+  t_CheckExitStatus 1
+fi
 
-t_CheckExitStatus $?
+t_CheckExitStatus 0
