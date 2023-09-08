8cacc73e75457547fb38f1c23e499bd34ac7b04c
Carlos Rodriguez-Fernandez <carlosrodrifernandez@gmail.com>
2023-09-07 17:26:53 -0700

    initscripts: redirect systemctl status output
    When running locally, the systemctl pager can come up
    in some circumstances, preventing the test run from
    continuing because it is waiting for stdin.
    
    The redirect prevents this from happening, and will
    output only in case of error for troubleshooting.
    
    Signed-off-by: Carlos Rodriguez-Fernandez <carlosrodrifernandez@gmail.com>

--- tests/p_initscripts/initscripts_service_status.sh
+++ tests/p_initscripts/initscripts_service_status.sh
@@ -8,8 +8,13 @@ fi
 
 t_Log "Running $0 - check if service cmd can get service status"
 
+output_file=$(mktemp)
+trap "rm -f ${output_file}" EXIT
+
 # auditd is used as example because it's standard with minimal install
-service auditd status 
+if ! service auditd status > ${output_file} 2>&1; then
+  cat ${output_file}
+  t_CheckExitStatus 1
+fi
 
-t_CheckExitStatus $?
-
+t_CheckExitStatus 0
